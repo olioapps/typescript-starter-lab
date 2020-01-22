46ca7a9bc03af759c186f652bc7a6021883bbe46
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2020-01-22 11:26:11 -0800

    add pet problems

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,60 @@
+const pets = [
+    {
+        name: "fido",
+        type: "dog",
+        dangerLevel: 2,
+    },
+    {
+        name: "rido",
+        type: "dog",
+        dangerLevel: 12,
+    },
+    {
+        name: "lido",
+        type: "dog",
+        dangerLevel: 4,
+    },
+    {
+        name: "dr. wizard",
+        type: "cat",
+        dangerLevel: 5,
+    },
+    {
+        name: "CatStevns",
+        type: "cat",
+        dangerLevel: 7,
+    },
+    {
+        name: "Dido",
+        type: "dog",
+        dangerLevel: 4,
+    },
+    {
+        name: "Lardo",
+        type: "locust",
+        dangerLevel: 11,
+    },
+]
+
+// what is the most dangerous animal?
+// 1. Write a function that returns the name of the most dangerous animal.
+// 2. Write a function that returns the name of the least dangerous animal.
+// 3. Write a function that returns an object containing only type and danger level of the most dangerous animal.
+
+// Who would win?
+// 1. Write a function that takes two numbers (0-6). The numbers represent the index of the pet in the pets array. Write a function that returns the animal which the highest danger level between the two pets.
+// 2. write a function that takes two names
+
+// how many of each pet type there is?
+// 1. Write a function that takes an array of pets (like the one defined at the top of this file) and returns a new object that shows the count of each type of animal. It should look like this:
+const petCount = {
+    dog: 4,
+    cat: 2,
+    locust: 1,
+    // [pet.type]: count
+}
+
+// New Array
+// write a function that returns an alphabetized list of animal names.
+
 console.log("TEST")
