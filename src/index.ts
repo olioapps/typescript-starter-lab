4750713f842e37892a9712f93c7848eb18ceae88
aron <aron@olioapps.com>
2021-09-30 10:21:33 -0700

    homework

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-console.log("TEST")
-console.log("HI AARON!")
+// create an interface called User
+// with these attributes
+//      id: string, optional
+//      name: string
+//      favoriteColor: string
+//      age: name
+
+// create a es6 class, using Typescript
+// where the methods are arrow operators
+// that has this method:
+// add user
+//      params: user of type User <-- object
+//      returns: user (with an assigned id)
+
+
+// create an instance of your class
+// invoke the add user method with a user object that has no id
+// the mthod
+//      creates a new copy of the user object, using spread operator, and assigns an id
+//      stores this new instance in a Record of type string, User, that exists as an instance variable (scoped to the class instance), private
+//      returns the new instance
+
+
+// show me via console log that the create operation worked
