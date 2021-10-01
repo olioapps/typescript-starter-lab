c1858dbc04384e7826a0304d14534b9ea86e50f2
awhitaker3 <awhitaker3@wisc.edu>
2021-10-01 08:02:01 -0700

    Created user interface and userAPI class with methods to add user

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,55 @@
 
 
 // show me via console log that the create operation worked
+
+interface User {
+    readonly id?: string
+    readonly name: string
+    readonly favoriteColor: string
+    readonly age: number 
+}
+
+class UserAPI{
+    private _userList : Record<string, User> 
+    private _idCounter : number
+    constructor(){
+        this._userList = {}
+        this._idCounter = 0
+    }
+    get userList(){
+        return Object.keys(this._userList).map((key => this._userList[key]))
+    }
+    addUser = (user: User) => {
+        this._idCounter += 1
+        const userCopy: User = {...user, id: this._idCounter.toString()}
+        this._userList[userCopy.id] = userCopy
+        return userCopy
+    }
+    getSingleUser(id: string){
+        return this._userList[id]
+    }
+    deleteUser(id: string){
+        delete this._userList[id]
+    }
+}
+
+const apiInstance = new UserAPI
+const user1 = {
+    name: "Jon",
+    favoriteColor: "Blue",
+    age: 22
+}
+const user2 = {
+    name: "Alex",
+    favoriteColor: "Green",
+    age: 50
+}
+let copy1 = apiInstance.addUser(user1)
+let copy2 = apiInstance.addUser(user2)
+console.log(user1)
+console.log(copy1)
+console.log(apiInstance.userList)
+console.log(apiInstance.getSingleUser('2'))
+console.log(apiInstance.getSingleUser('5'))
+apiInstance.deleteUser('1')
+console.log(apiInstance.userList)
