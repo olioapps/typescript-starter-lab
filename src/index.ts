5958419477bdae43b59ba966fdd41fc14ead1393
Ian Andrewson <ianandrewson@gmail.com>
2021-12-09 15:33:44 -0800

    lab skeleton written in index.ts.

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+interface User {
+  id?: number,
+  name: string,
+  age: number,
+  color: string
+}
+
+class UserApi {
+  users: User[] = [];
+  
+  getUser(id: number): User {
+
+  }
+
+  getUsers(): User[] {
+
+  }
+
+  createUser(obj: User): User {
+
+  }
+
+  deleteUser(id: number): User {
+
+  }
+
+  updateUser(id: number): User {
+    
+  }
+};
+
