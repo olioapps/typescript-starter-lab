a949b879b3b80b6f4d4acc6ac3ce93093339eaaf
Ian Andrewson <ianandrewson@gmail.com>
2021-12-10 14:18:31 -0800

    test scaffolding complete in index.test.ts.

---
+++ src/index.test.ts
@@ -0,0 +1,58 @@
+import { User, ID, Color, UserApi } from "./index"
+
+describe("UserApi tests", () => {
+  console.log("Starting UserApi test")
+
+  let user1: User
+  let user2: User
+  let user3: User
+  let api = new UserApi()
+
+  it("Can initialize a new UserApi", () => {
+
+  })
+
+  it("Can create a new user", () => {
+
+  })
+
+  it("Can get all users", () => {
+
+  })
+
+  it("Can get a single user", () => {
+
+  })
+
+  it("Can update a user", () => {
+
+  })
+
+  it("Can delete a user", () => {
+
+  })
+
+  it("Throws when fetching a user without supplying an ID", () => {
+
+  })
+
+  it("Throws when creating a user with an already existing ID", () => {
+
+  })
+
+  it("Throws when deleting a user without supplying an ID", () => {
+
+  })
+
+  it("Throws when updating a user without suppling an ID", () => {
+
+  })
+
+  it("Throws when attempting to update a user that does not exist", () => {
+
+  })
+
+  it("Throws when deleting a user that does not exist", () => {
+
+  })
+})
