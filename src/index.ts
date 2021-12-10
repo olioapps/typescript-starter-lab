122ee954896230ca8877824229b2e10f076db4b7
Ian Andrewson <ianandrewson@gmail.com>
2021-12-10 12:25:32 -0800

    constrained favoriteColor to a few colors. Users is private. generateId is private. Removed as User from update and createUser. Linted.

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,64 @@
 interface User {
-  id?: number,
-  name: string,
-  age: number,
-  favoriteColor: string
+  readonly id?: number,
+  readonly name: string,
+  readonly age: number,
+  readonly favoriteColor: Color
 }
 
 type ID = number;
+type Color = "red" | "green" | "blue"
 
 class UserApi {
-  users = new Map<ID, User>();
-  private static count = 0;
+  private users = new Map<ID, User>()
+  private static count = 0
 
-  static generateId(): ID {
-    UserApi.count++;
-    return UserApi.count;
+  private static generateId(): ID {
+    UserApi.count++
+    return UserApi.count
   }
   
-  getUser(id: ID): User | null {
-    if(!id) throw new Error("Missing user ID.");
+  getUser = (id: ID): User | null => {
+    if(!id) { throw new Error("Missing user ID.") }
     return this.users.get(id) || null
   }
 
-  getUsers(): User[] {
-    const result: User[] = [];
-    for(let [id, user] of this.users) {
-      result.push(user);
+  getUsers = (): User[]  => {
+    // to replace with iterator
+    const result: User[] = []
+    for (let [id, user] of this.users) {
+      result.push(user)
     }
-    return result;
+    return result
   }
 
-  createUser(newUser: User): User {
-    if(newUser.id && this.users.get(newUser.id)) {
-      throw new Error("User already exists.");
+  createUser = (newUser: User): User => {
+    if (newUser.id && this.users.get(newUser.id)) {
+      throw new Error("User already exists.")
     }
-    const id = UserApi.generateId(); 
-    this.users.set(id, { id, ...newUser})
-    return this.users.get(id) as User;
+    const id = UserApi.generateId()
+    const userObject = { id, ...newUser}
+    this.users.set(id, userObject)
+    return userObject
   }
 
-  deleteUser(id: ID): Boolean {
-    if(!id) throw new Error("Missing User ID")
-    return this.users.delete(id);
+  deleteUser = (id: ID): boolean => {
+    if (!id) { throw new Error("Missing User ID") }
+    return this.users.delete(id)
   }
 
-  updateUser(newData: User): User {
-    if(!newData.id) throw new Error('Missing User ID.');
+  updateUser = (newData: User): User => {
+    if (!newData.id) { throw new Error('Missing User ID.') }
     const existingUser = this.users.get(newData.id)
-    if(!existingUser) throw new Error('User not found.');
+    if (!existingUser) { throw new Error('User not found.') }
 
     //as PATCH:
     // const updatedUser = { ...existingUser, ...newData }
 
     //as PUT:
     this.users.set(newData.id, newData)
-    return this.users.get(newData.id) as User;
+    return newData
   }
-};
+}
 
 console.log('Initializing UserApi.');
 const users = new UserApi();
@@ -72,7 +75,7 @@ console.log(users.getUsers());
 console.log('Fetching a user...')
 console.log(users.getUser(user1.id));
 console.log('Updating a user...');
-users.updateUser({ ...user1, favoriteColor: 'orange'});
+users.updateUser({ ...user1, favoriteColor: 'green'});
 console.log('Updated user: ', users.getUser(user1.id));
 console.log('Deleting a user...');
 users.deleteUser(user1.id);
