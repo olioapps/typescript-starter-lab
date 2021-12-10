ba737c14f40a51957a88ce1272ab14f8cccfda97
Ian Andrewson <ianandrewson@gmail.com>
2021-12-10 12:26:35 -0800

    linted single quotes to double.

--- src/index.ts
+++ src/index.ts
@@ -47,9 +47,9 @@ class UserApi {
   }
 
   updateUser = (newData: User): User => {
-    if (!newData.id) { throw new Error('Missing User ID.') }
+    if (!newData.id) { throw new Error("Missing User ID.") }
     const existingUser = this.users.get(newData.id)
-    if (!existingUser) { throw new Error('User not found.') }
+    if (!existingUser) { throw new Error("User not found.") }
 
     //as PATCH:
     // const updatedUser = { ...existingUser, ...newData }
@@ -60,48 +60,48 @@ class UserApi {
   }
 }
 
-console.log('Initializing UserApi.');
+console.log("Initializing UserApi.");
 const users = new UserApi();
-console.log('initialized Users map: ', users.getUsers());
-console.log('Creating a couple users...')
-const user1 = users.createUser({ name: 'Ian', age: 31, favoriteColor: 'red' });
-console.log('Created user: ', user1);
-const user2 = users.createUser({ name: 'Evan', age: 29, favoriteColor: 'green' });
-console.log('Created user: ', user2);
-const user3 = users.createUser({ name: 'Sarah', age: 29, favoriteColor: 'blue' });
-console.log('Created user: ', user3);
-console.log('Fetching all users...');
+console.log("initialized Users map: ", users.getUsers());
+console.log("Creating a couple users...")
+const user1 = users.createUser({ name: "Ian", age: 31, favoriteColor: "red" });
+console.log("Created user: ", user1);
+const user2 = users.createUser({ name: "Evan", age: 29, favoriteColor: "green" });
+console.log("Created user: ", user2);
+const user3 = users.createUser({ name: "Sarah", age: 29, favoriteColor: "blue" });
+console.log("Created user: ", user3);
+console.log("Fetching all users...");
 console.log(users.getUsers());
-console.log('Fetching a user...')
+console.log("Fetching a user...")
 console.log(users.getUser(user1.id));
-console.log('Updating a user...');
-users.updateUser({ ...user1, favoriteColor: 'green'});
-console.log('Updated user: ', users.getUser(user1.id));
-console.log('Deleting a user...');
+console.log("Updating a user...");
+users.updateUser({ ...user1, favoriteColor: "green"});
+console.log("Updated user: ", users.getUser(user1.id));
+console.log("Deleting a user...");
 users.deleteUser(user1.id);
-console.log('Users after deleting: ', users.getUsers());
-console.log('Attempting to get a deleted user...');
+console.log("Users after deleting: ", users.getUsers());
+console.log("Attempting to get a deleted user...");
 console.log(users.getUser(user1.id));
 try {
-  console.log('Attempting to fetch a user without supplying an ID...');
+  console.log("Attempting to fetch a user without supplying an ID...");
   users.getUser();
 } catch(err) { console.log(err.message) }
 try {
-  console.log('Attempting to create a user that already exists')
+  console.log("Attempting to create a user that already exists")
   users.createUser(user2);
 } catch(err) { console.log(err.message) }
 try {
-  ('Attempting to delete a user without supplying an ID...');
+  ("Attempting to delete a user without supplying an ID...");
   users.deleteUser();
 } catch(err) { console.log(err.message) }
 try {
-  console.log('Attempting to update a user without supplying an ID...');
+  console.log("Attempting to update a user without supplying an ID...");
   const fakeUser = { ...user3 };
   delete fakeUser.id;
   users.updateUser(fakeUser)
 } catch(err) { console.log(err.message) }
 try {
-  console.log('Attempting to update a user that does not exist...')
+  console.log("Attempting to update a user that does not exist...")
   users.updateUser({ ...user2, id: 99 });
 } catch(err) { console.log(err.message) }
-console.log('Done.');
+console.log("Done.");
