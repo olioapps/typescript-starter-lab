6a9005f86943f4f78cb1dbaded0f78dd56d2f4d5
Ian Andrewson <ianandrewson@gmail.com>
2021-12-09 16:21:47 -0800

    first implementation of gettUser and getUsers implemented.

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,16 @@ type ID = number;
 class UserApi {
   users = new Map<ID, User>();
   
-  getUser(id: ID): User {
-
+  getUser(id: ID): User | undefined {
+    return this.users.get(id)
   }
 
   getUsers(): User[] {
-
+    const result: User[] = [];
+    for(let [id, user] of this.users) {
+      result.push(user);
+    }
+    return result;
   }
 
   createUser(obj: User): User {
