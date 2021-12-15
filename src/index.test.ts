da41bb3289c8fa5de3d08e1a10eb030ecc456de2
Ian Andrewson <ianandrewson@gmail.com>
2021-12-15 13:59:44 -0800

    added factory function to index.test.js to instantiate class based on input string. Made new class ArrayUserApi that uses ReadonlyArray instead of map. All tests pass with new class after changing tests a bit to fix some false positives: tests that test for an error now check to make sure the operation will throw before entering the try catch block.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,115 +1,133 @@
-import { User, ApiError, UserApi } from "./index";
+import { User, ApiError, UserApi, ArrayUserApi, Color,  } from "./index";
 
-describe("UserApi tests", () => {
-  console.log("Starting UserApi test")
+type apiClass = UserApi | ArrayUserApi
+type classNameStrs = "api1" | "api2"
+const makeClass = (className: classNameStrs): apiClass => {
+  return className === "api1" ? new UserApi : new ArrayUserApi
+}
 
-  let user1: User
-  let user2: User
-  let user3: User
-  let api = new UserApi
+const testMe = (classStr: classNameStrs) => {
+  describe("UserApi tests", () => {
 
-  it("Can initialize a new UserApi", () => {
-    const testApi = new UserApi();
-    expect(testApi).toBeInstanceOf(UserApi);
-  })
+    let user1: User
+    let user2: User
+    let user3: User
+    let api = makeClass(classStr)
 
-  it("Can create a new user", () => {
-    user1 = api.createUser({ name: "Ian", age: 31, favoriteColor: "red" })
-    expect(user1).toEqual({
-      id: 1,
-      name: "Ian",
-      age: 31,
-      favoriteColor: "red"
+    it("Can initialize a new UserApi", () => {
+      const testApi = makeClass(classStr)
+      expect(testApi).toBeDefined();
+    })
+
+    it("Can create a new user", () => {
+      user1 = api.createUser({ name: "Ian", age: 31, favoriteColor: "red" })
+      expect(user1).toEqual({
+        id: 1,
+        name: "Ian",
+        age: 31,
+        favoriteColor: "red"
+      })
+    })
+
+    it("Can get all users", () => {
+      user2 = api.createUser({ name: "Evan", age: 29, favoriteColor: "green" })
+      user3 = api.createUser({ name: "Sarah", age: 29, favoriteColor: "blue" })
+
+      const users = api.getUsers();
+      expect(users).toEqual([
+        { id: 1, name: "Ian", age: 31, favoriteColor: "red" },
+        { id: 2, name: "Evan", age: 29, favoriteColor: "green"} ,
+        { id: 3, name: "Sarah", age: 29, favoriteColor: "blue" }
+      ])
+    })
+
+    it("Can get a single user", () => {
+      const result = api.getUser(1);
+      expect(result).toEqual({ ...user1, id: 1})
+    })
+
+    it("returns null if a user cannot be found by ID", () => {
+      const result = api.getUser(99)
+      expect(result).toBeNull()
+    })
+
+    it("Can update a user", () => {
+      api.updateUser({ id: 1, name: "Ian", age: 31, favoriteColor: "blue"})
+
+      expect(api.getUser(1)).toEqual({
+        id: 1,
+        name: "Ian",
+        age: 31,
+        favoriteColor: "blue"
+      })
+    })
+
+    it("Can delete a user", () => {
+      api.deleteUser(1)
+      expect(api.getUsers().length).toEqual(2)
+    })
+
+    it("Throws when creating a user with an already existing ID", () => {
+      const badCreate = {id: 2, name: "Bob", age: 56, favoriteColor: "red" as Color}
+      expect(() => api.createUser(badCreate)).toThrow()
+      try {
+        api.createUser(badCreate)
+      } catch (error: any) {
+        expect(error).toBeInstanceOf(ApiError)
+        expect(error.message).toEqual("User already exists.")
+        expect(error.status).toEqual(409)
+      }
+    })
+
+    it("Throws when updating a user without supplying an ID", () => {
+      const badUpdate = { name: "Evan", age: 20 }
+      //@ts-ignore
+      expect(() => api.updateUser(badUpdate)).toThrow()
+      try{
+        // @ts-ignore
+        api.updateUser(badUpdate)
+      } catch (error: any) {
+        expect(error).toBeInstanceOf(ApiError)
+        expect(error.message).toEqual("Missing User ID.")
+        expect(error.status).toEqual(400)
+      }
+    })
+
+    it("Throws when attempting to update a user that does not exist", () => {
+      const badUser = { id: 7, name: "Erica" }
+      //@ts-ignore
+      expect(() => api.updateUser(badUser)).toThrow()
+      try{
+        // @ts-ignore
+        api.updateUser(badUser)
+      } catch (error: any) {
+        expect(error).toBeInstanceOf(ApiError)
+        expect(error.message).toEqual("User not found.")
+        expect(error.status).toEqual(404)
+      }
+    })
+
+    it("Throws when deleting a user that does not exist", () => {
+      expect(() => api.deleteUser(20)).toThrow()
+      try{
+      // @ts-ignore
+      api.deleteUser(20)
+      } catch (error: any) {
+        expect(error).toBeInstanceOf(ApiError)
+        expect(error.message).toEqual("User not found.")
+        expect(error.status).toEqual(404)
+      }
+    })
+
+    it("Can construct a new ApiError", () => {
+      const error = new ApiError("Test", 404)
+      expect(error).toBeInstanceOf(ApiError)
+      expect(error.message).toEqual("Test")
+      expect(error.status).toEqual(404)
+      expect(error.stack).toBeDefined();
     })
   })
+}
 
-  it("Can get all users", () => {
-    user2 = api.createUser({ name: "Evan", age: 29, favoriteColor: "green" })
-    user3 = api.createUser({ name: "Sarah", age: 29, favoriteColor: "blue" })
-
-    const users = api.getUsers();
-    expect(users).toEqual([
-      { id: 1, name: "Ian", age: 31, favoriteColor: "red" },
-      { id: 2, name: "Evan", age: 29, favoriteColor: "green"} ,
-      { id: 3, name: "Sarah", age: 29, favoriteColor: "blue" }
-    ])
-  })
-
-  it("Can get a single user", () => {
-    const result = api.getUser(1);
-    expect(result).toEqual({ ...user1, id: 1})
-  })
-
-  it("returns null if a user cannot be found by ID", () => {
-    const result = api.getUser(99)
-    expect(result).toBeNull()
-  })
-
-  it("Can update a user", () => {
-    api.updateUser({ id: 1, name: "Ian", age: 31, favoriteColor: "blue"})
-
-    expect(api.getUser(1)).toEqual({
-      id: 1,
-      name: "Ian",
-      age: 31,
-      favoriteColor: "blue"
-    })
-  })
-
-  it("Can delete a user", () => {
-    api.deleteUser(1)
-    expect(api.getUsers().length).toEqual(2)
-  })
-
-  it("Throws when creating a user with an already existing ID", () => {
-    try {
-      api.createUser({id: 2, name: "Bob", age: 56, favoriteColor: "red"})
-    } catch (error: any) {
-      expect(error).toBeInstanceOf(ApiError)
-      expect(error.message).toEqual("User already exists.")
-      expect(error.status).toEqual(409)
-    }
-  })
-
-  it("Throws when updating a user without supplying an ID", () => {
-    try{
-      // @ts-ignore
-      api.updateUser({ name: "Evan", age: 20 })
-    } catch (error: any) {
-      expect(error).toBeInstanceOf(ApiError)
-      expect(error.message).toEqual("Missing User ID.")
-      expect(error.status).toEqual(400)
-    }
-  })
-
-  it("Throws when attempting to update a user that does not exist", () => {
-    try{
-      // @ts-ignore
-      api.updateUser({ id: 7, name: "Erica" })
-    } catch (error: any) {
-      expect(error).toBeInstanceOf(ApiError)
-      expect(error.message).toEqual("User not found.")
-      expect(error.status).toEqual(404)
-    }
-  })
-
-  it("Throws when deleting a user that does not exist", () => {
-    try{
-    // @ts-ignore
-    api.deleteUser(20)
-    } catch (error: any) {
-      expect(error).toBeInstanceOf(ApiError)
-      expect(error.message).toEqual("User not found.")
-      expect(error.status).toEqual(404)
-    }
-  })
-
-  it("Can construct a new ApiError", () => {
-    const error = new ApiError("Test", 404)
-    expect(error).toBeInstanceOf(ApiError)
-    expect(error.message).toEqual("Test")
-    expect(error.status).toEqual(404)
-    console.log(JSON.stringify(error))
-    expect(error.stack).toBeDefined();
-  })
-})
+testMe("api1")
+testMe("api2")
--- src/index.ts
+++ src/index.ts
@@ -51,9 +51,9 @@ export class UserApi {
     this.users.set(newData.id, newData)
     return newData
   }
-};
+}
 
-export class ApiError extends Error{
+export class ApiError extends Error {
   status: number;
 
   constructor(message: string, status: number) {
@@ -65,3 +65,58 @@ export class ApiError extends Error{
     }
   }
 }
+
+export class ArrayUserApi {
+  private users: ReadonlyArray<User> = []
+  private static count = 0
+
+  private static generateId(): ID {
+    ArrayUserApi.count++
+    return ArrayUserApi.count
+  }
+  
+  getUser = (id: ID): User | null => {
+    return this.users.reduce((acc, curr) => {
+      if (curr.id === id ) acc = curr
+      return acc
+    }, null as User | null)
+  }
+
+  getUsers = (): readonly User[]  => {
+    return this.users
+  }
+
+  createUser = (newUser: User): User => {
+    if (newUser.id && this.getUser(newUser.id)) {
+      throw new ApiError("User already exists.", 409)
+    }
+    const id = ArrayUserApi.generateId()
+    const userObject = { id, ...newUser}
+    this.users = [ ...this.users, userObject ]
+    return userObject
+  }
+
+  deleteUser = (id: ID): void => {
+    if (!this.getUser(id)) { throw new ApiError("User not found.", 404)}
+    this.users = this.users.filter(user => user.id != id)
+  }
+
+  updateUser = (newData: User): User => {
+    if (!newData.id) { throw new ApiError("Missing User ID.", 400) }
+    const existingUser = this.getUser(newData.id)
+    if (!existingUser) { throw new ApiError("User not found.", 404) }
+
+    //as PATCH:
+    // const updatedUser = { ...existingUser, ...newData }
+
+    //as PUT:
+    // this.users.set(newData.id, newData)
+    this.users = this.users.map(user => {
+      if (user.id === newData.id) return newData
+      else return user
+    })
+    return newData
+  }
+}
+
+export const blah = 'blah'
