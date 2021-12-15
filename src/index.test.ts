2126ce7e835133741d5989018051d51bde5babb3
Michelle Stermitz <stermitzm@gmail.com>
2021-12-15 15:56:41 -0800

    Create UserAPI class, stub methods and tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,41 @@
 //write tests here
+import { UserAPI } from "./index"
+
+const users = new UserAPI()
+console.log("USERS", users)
 
 describe('Tests will go here!', () => {
   it('should pass', () => {
     expect(true).toBeTruthy()
+  })
+
+  it('gets a user by id', () => {
+    const result = users.getUserById()
+
+    expect(result).toStrictEqual(1) 
+  })
+
+  it('gets all users', () => {
+    const result = users.getUsers()
+
+    expect(result).toStrictEqual(1) 
+  }) 
+
+  it('creates a user', () => {
+    const result = users.createUser()
+
+    expect(result).toStrictEqual(1) 
+  })
+
+  it('deletes a user by id', () => {
+    const result = users.deleteUserById()
+
+    expect(result).toStrictEqual(1) 
+  })
+
+  it('updates a user by id', () => {
+    const result = users.updateUser()
+
+    expect(result).toStrictEqual(1) 
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,65 @@
 //Define class here
+
+interface User {
+    readonly id?: number
+    readonly name: string
+    readonly age: number
+    readonly color: string
+}
+
+export class UserAPI {
+    users: ReadonlyArray<User>
+
+    constructor() {
+        this.users = [
+            {
+                id: 1,
+                name: "Michelle",
+                age: 30,
+                color: "rainbow",
+            },
+            {
+                id: 2,
+                name: "Vintage Aaron",
+                age: 35,
+                color: "blue",
+            },
+            {
+                id: 3,
+                name: "Derek",
+                age: 28,
+                color: "green",
+            },
+            {
+                name: "George",
+                age: 32,
+                color: "red",
+            },
+        ]
+    }
+
+    public getUserById = () => {
+        console.log("Get user by id")
+        return 1
+    } 
+
+    public getUsers = () => {
+        console.log("Get users")
+        return 1
+    }
+
+    public createUser = () => {
+        console.log("Create user")
+        return 1
+    }
+
+    public deleteUserById = () => {
+        console.log("Create user by id")
+        return 1
+    }
+
+    public updateUser = () => {
+        console.log("Update user")
+        return 1
+    }
+ }
