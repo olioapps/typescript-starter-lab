b5cfc01f4374d6ee00220af04d7a19e208102716
Ian Andrewson <ianandrewson@gmail.com>
2021-12-16 09:45:46 -0800

    scoreTracker implemented. Paushing to fix branches and commit history.

--- src/eventStream.ts
+++ src/eventStream.ts
@@ -6,19 +6,27 @@ export interface StreamEvent {
 }
 
 interface ScoreTracker {
-  subarrayScore: number,
+  highScore: number,
+  currentScore: number,
   startIndex: number
 }
 
 export const scoreArray = (eventArray: StreamEvent[]): StreamEvent[] => {
+  const scoreTracker: ScoreTracker = {
+    highScore: 0,
+    currentScore: 0,
+    startIndex: 0
+  }
+
+  
+  //keep track of highest score so far and starting index of subarray
+  //look at first five, set to current score and high score
+    //use pointer to keep track of position in array
+  //subtract score at pointer
+  //increment pointer
+  //add score of (index of pointer + 5)
+  //calc, compare, repeat
   const scores = eventArray.map(streamEvent => {
-    //keep track of highest score so far and starting index of subarray
-    //look at first five, set to current score and high score
-      //use pointer to keep track of position in array
-    //subtract score at pointer
-    //increment pointer
-    //add score of (index of pointer + 5)
-    //calc, compare, repeat
     return streamEvent
   })
 
