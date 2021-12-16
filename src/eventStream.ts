9675fbcc1c0baf68e7ecefd0ba454bad4a03abba
Ian Andrewson <ianandrewson@gmail.com>
2021-12-16 09:18:32 -0800

    merge conflicts resolved?

---
+++ src/eventStream.ts
@@ -0,0 +1,26 @@
+type EventType = "screenshot" | "new message" | "view"
+
+export interface StreamEvent {
+  timestamp: number,
+  eventType: EventType
+}
+
+interface ScoreTracker {
+  subarrayScore: number,
+  startIndex: number
+}
+
+export const scoreArray = (eventArray: StreamEvent[]): StreamEvent[] => {
+  const scores = eventArray.map(streamEvent => {
+    //keep track of highest score so far and starting index of subarray
+    //look at first five, set to current score and high score
+      //use pointer to keep track of position in array
+    //subtract score at pointer
+    //increment pointer
+    //add score of (index of pointer + 5)
+    //calc, compare, repeat
+    return streamEvent
+  })
+
+  return scores
+}
