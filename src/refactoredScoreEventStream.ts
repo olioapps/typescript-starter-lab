2f4d037856071295f95bd700492d9ad3125dc4c3
Laurie Waller <lauriewaller7@gmail.com>
2022-02-08 17:27:28 -0800

    add sum helperfunction

--- src/refactoredScoreEventStream.ts
+++ src/refactoredScoreEventStream.ts
@@ -1,34 +1,43 @@
 import { Event } from "./models"
-
-const scoreFirstFiveSubarrays = (eventStream: Event[]): number => { 
-  let sum: number = 0 
-  for (let i=0; i < 5; i++) {
-    eventStream[i].eventType === 'new message' 
-      ? sum += 1 
-      : eventStream[i].eventType === 'view' 
-        ? sum += 2 
-        : sum += 3
-  }
-  return sum
+   //add tests to helper functions 
+  //check typing throughout 
+const sumArray = (eventArray: any) => {
+  return eventArray.reduce((sum: number, event: Event): number => {
+    return event.eventType === 'new message' 
+      ? (sum += 1)
+      : event.eventType === 'view' 
+        ? (sum += 2)
+        : (sum += 3)
+  }, 0)
 }
 
-const scoreRemainingSubarrays = (eventStreamSlice: Event[], subarraySum: number) => {
+const scoreFirstFiveSubarrays = (eventStream: Event[]): any => {  
+  const firstFiveElements = eventStream.slice(0, 4);
+  return sumArray(firstFiveElements);
+}
+
+const scoreRemainingSubarrays = (
+  eventStream: Event[],
+  subarraySum: number,
+): number => { 
   let highScoreFinalIndex = 4 // update?  
   let subarraySumToCompare = subarraySum  
-  for (let i = 5; i < eventStreamSlice.length; i++) { 
+  for (let i = 5; i < eventStream.length; i++) { 
       let lastEventValue = 0
-      eventStreamSlice[i].eventType === 'new message' 
-        ? subarraySumToCompare += 1 
-        : eventStreamSlice[i].eventType === 'view' 
-          ? subarraySumToCompare += 2 
-          : subarraySumToCompare += 3
 
-        eventStreamSlice[i-5].eventType === 'new message' 
-        ? lastEventValue += 1 
-        : eventStreamSlice[i-5].eventType === 'view' 
-          ? lastEventValue += 2 
-          : lastEventValue += 3
-        subarraySumToCompare -= lastEventValue
+    eventStream[i].eventType === 'new message' //DRY this code 
+      ? subarraySumToCompare += 1 
+      : eventStream[i].eventType === 'view' 
+        ? subarraySumToCompare += 2 
+        : subarraySumToCompare += 3
+//(eventType, currentSum)
+      eventStream[i-5].eventType === 'new message' 
+      ? lastEventValue += 1 
+      : eventStream[i-5].eventType === 'view' 
+        ? lastEventValue += 2 
+        : lastEventValue += 3
+
+      subarraySumToCompare -= lastEventValue
 
     if (subarraySumToCompare > subarraySum) {
       highScoreFinalIndex = i
@@ -47,11 +56,13 @@ export function scoreEventStream(eventStream: Event[]): Event[] {
     let subarraySum = scoreFirstFiveSubarrays(eventStream) 
 
     const index = scoreRemainingSubarrays(eventStream, subarraySum)
-
     const highScoreSubarrayFirstIndex = index - 4 
     const highScoreSubarrayLastIndex = index + 1
 
-    const finalHighScoreSubarray: Event[] = eventStream.slice(highScoreSubarrayFirstIndex, highScoreSubarrayLastIndex)
+    const finalHighScoreSubarray: Event[] = eventStream.slice(
+      highScoreSubarrayFirstIndex, 
+      highScoreSubarrayLastIndex,
+    )
 
     return finalHighScoreSubarray
   }
