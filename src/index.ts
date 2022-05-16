1837c35a77d862faeb4ef971e49b4223a004f6f4
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-05-16 15:43:08 -0700

    have one skeloton of for add User

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 //Define class here
 console.log("hello there!");
 
-class UserApi {}
+class UserAPI {
+  constructor() {}
+  // these are the methods needed for basic CRUD
+  addUser(name) {
+    console.log(`inside addUser ${name}`);
+  }
+}
+
+const users = new UserAPI();
+
+users.addUser("minoka");
