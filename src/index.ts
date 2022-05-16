977a93c7aa7f1d136a0285515b7075dc876eed51
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-05-16 15:57:07 -0700

    added skeleton for each crud operation

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,23 @@ class UserAPI {
   addUser(name) {
     console.log(`inside addUser ${name}`);
   }
+  getUserById(id) {
+    console.log(`looking for user with Id of ${id}`);
+  }
+  getUsers() {
+    console.log("returns an array of users");
+  }
+  deleteUserById(id) {
+    console.log(`deletes user with id ${id}`);
+  }
+  searchUserByName(name) {
+    console.log(`returns users which included ${name}`);
+  }
 }
 
 const users = new UserAPI();
-
 users.addUser("minoka");
+users.getUserById(3);
+users.getUsers();
+users.deleteUserById(1);
+users.searchUserByName("mino");
