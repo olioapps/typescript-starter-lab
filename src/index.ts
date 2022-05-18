93a16dce5a089a7409451d727a001fa3b83e2070
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-05-18 11:18:16 -0700

    changed the mockdata ids to strings, updateUserById is working with two errors checking. if there is a user id and if there is a user with that id in the list.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 //Define class here
 const mockUsers = [
-  { id: 0, name: "minoka", age: 31, favoriteColor: "green" },
-  { id: 1, name: "ted", age: 23, favoriteColor: "black" },
-  { id: 2, name: "ron", age: 41, favoriteColor: "blue" },
-  { id: 3, name: "tim", age: 27, favoriteColor: "red" },
+  { id: "0", name: "minoka", age: 31, favoriteColor: "green" },
+  { id: "1", name: "ted", age: 23, favoriteColor: "black" },
+  { id: "2", name: "ron", age: 41, favoriteColor: "blue" },
+  { id: "3", name: "tim", age: 27, favoriteColor: "red" },
 ]
 
 class UserAPI {
@@ -14,11 +14,11 @@ class UserAPI {
   randomId = () => {
     return Math.random().toString(24).slice(2)
   }
-  // these are the methods needed for basic CRUD
+
   addUser(user) {
     try {
       if (!user.name)
-        throw new Error("you need to at least have a name to add a user")
+        throw new Error("You need to at least have a name to add a user")
       const newUser = { id: this.randomId(), ...user }
       this.list.push(newUser)
       return newUser
@@ -35,8 +35,25 @@ class UserAPI {
       err.status = 404
     }
   }
-  updateUserById(id) {
-    console.log(`updates user with Id${id}`)
+  updateUserById(updatedUser) {
+    try {
+      if (!updatedUser.id)
+        throw new Error("We can not update a user without an id")
+      const updatedList = this.list.map((user) => {
+        if (user.id === updatedUser.id) {
+          return { ...user, ...updatedUser }
+        }
+        return user
+      })
+
+      this.list = updatedList
+      const targetUser = this.list.find((user) => user.id === id)
+      if (!targetUser) throw new Error("There are no users found with that id.")
+      return targetUser
+    } catch (err) {
+      err.status = 400
+      console.log(err)
+    }
   }
   getUsers() {
     console.log("returns an array of users")
@@ -49,22 +66,43 @@ class UserAPI {
   }
 }
 const userObject = { name: "minoka", age: 31, favoriteColor: "green" }
+const updatingUserObject = {
+  id: "0",
+  name: "minoka",
+  age: 100,
+  favoriteColor: "red",
+}
+const updatingUserObjectError = {
+  name: "minoka",
+  age: 100,
+  favoriteColor: "red",
+}
+const updatingUserObjectErrorWithId = {
+  id: "124324614614331461346",
+  name: "minoka",
+  age: 100,
+  favoriteColor: "red",
+}
 
 const users = new UserAPI(mockUsers)
-console.log("addUser:", users.addUser(userObject))
-console.log("addUser error:", users.addUser({}))›
-console.log("-------------------------------------")
+// console.log("addUser:", users.addUser(userObject))
+// console.log("addUser error:", users.addUser({}))
+// console.log("-------------------------------------")
 
-console.log("getUserById:", users.getUserById(3))
-console.log("getUserById error:", users.getUserById(5))
-console.log("-------------------------------------")
+// console.log("getUserById:", users.getUserById("3"))
+// console.log("getUserById error:", users.getUserById("5"))
+// console.log("-------------------------------------")
 
-const emptyUsers = new UserAPI([])
-emptyUsers.addUser(userObject)
-emptyUsers.addUser(userObject)
-emptyUsers.addUser(userObject)
-console.log("emptyUsers", emptyUsers)
-// users.updateUserById(3);
+// const emptyUsers = new UserAPI([])
+// emptyUsers.addUser(userObject)
+// emptyUsers.addUser(userObject)
+// emptyUsers.addUser(userObject)
+// console.log("emptyUsers", emptyUsers)
+// console.log("-------------------------------------")
+
+console.log(users.updateUserById(updatingUserObject))
+console.log(users.updateUserById(updatingUserObjectError))
+console.log(users.updateUserById(updatingUserObjectErrorWithId))
 // users.getUsers();›
 // users.deleteUserById(1);
 // users.searchUserByName("mino");
