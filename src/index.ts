83d8c36b67cd690315f639685365e8b43972f7ec
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-05-18 09:38:19 -0700

    added try catches to catch the errors thrown, when making a user you need to at least have a name to continue.

--- src/index.ts
+++ src/index.ts
@@ -4,55 +4,63 @@ const mockUsers = [
   { id: 1, name: "ted", age: 23, favoriteColor: "black" },
   { id: 2, name: "ron", age: 41, favoriteColor: "blue" },
   { id: 3, name: "tim", age: 27, favoriteColor: "red" },
-];
+]
 
 class UserAPI {
   constructor(users) {
-    this.list = users || [];
+    this.list = users || []
   }
   //random id generator
   randomId = () => {
-    return Math.random().toString(24).slice(2);
-  };
+    return Math.random().toString(24).slice(2)
+  }
   // these are the methods needed for basic CRUD
   addUser(user) {
-    const newUser = { id: this.randomId(), ...user };
-    this.list.push(newUser);
-    return newUser;
+    try {
+      if (!user.name)
+        throw new Error("you need to at least have a name to add a user")
+      const newUser = { id: this.randomId(), ...user }
+      this.list.push(newUser)
+      return newUser
+    } catch (err) {
+      err.status = 406
+    }
   }
   getUserById(id) {
-    const targetUser = this.list.find((user) => user.id === id);
-    if (!targetUser) {
-      return new Error("There are no users found with that id.");
-    } else {
-      return targetUser;
+    const targetUser = this.list.find((user) => user.id === id)
+    try {
+      if (!targetUser) throw new Error("There are no users found with that id.")
+      return targetUser
+    } catch (err) {
+      err.status = 404
     }
   }
   updateUserById(id) {
-    console.log(`updates user with Id${id}`);
+    console.log(`updates user with Id${id}`)
   }
   getUsers() {
-    console.log("returns an array of users");
+    console.log("returns an array of users")
   }
   deleteUserById(id) {
-    console.log(`deletes user with id ${id}`);
+    console.log(`deletes user with id ${id}`)
   }
   searchUserByName(name) {
-    console.log(`returns users which included ${name}`);
+    console.log(`returns users which included ${name}`)
   }
 }
-const userObject = { name: "minoka", age: 31, favoriteColor: "green" };
+const userObject = { name: "minoka", age: 31, favoriteColor: "green" }
 
-const users = new UserAPI(mockUsers);
-console.log("addUser:", users.addUser(userObject));
-console.log("getUserById:", users.getUserById(3));
-console.log("getUserById error:", users.getUserById(5));
+const users = new UserAPI(mockUsers)
+console.log("addUser:", users.addUser(userObject))
+console.log("addUser error:", users.addUser({}))
+console.log("getUserById:", users.getUserById(3))
+console.log("getUserById error:", users.getUserById(5))
 
-const emptyUsers = new UserAPI();
-emptyUsers.addUser(userObject);
-emptyUsers.addUser(userObject);
-emptyUsers.addUser(userObject);
-console.log("emptyUsers", emptyUsers);
+const emptyUsers = new UserAPI()
+emptyUsers.addUser(userObject)
+emptyUsers.addUser(userObject)
+emptyUsers.addUser(userObject)
+console.log("emptyUsers", emptyUsers)
 // users.updateUserById(3);
 // users.getUsers();
 // users.deleteUserById(1);
