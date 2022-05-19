6cb6858e9d3f918f4bc7a9c6710ff1b77736a27d
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-05-19 15:14:22 -0700

    moved mock data to a different file and wrote a skeleton of the add users test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,17 @@
 //write tests here
+import UserAPI from "./index"
+import {
+  mockUsers,
+  userObject,
+  updatingUserObject,
+  updatingUserObjectError,
+  updatingUserObjectErrorWithId,
+} from "./mockdata"
 
-describe('Tests will go here!', () => {
-  it('should pass', () => {
+describe("Testing User API", () => {
+  it("should add a user to and throw an error if no name is provided", () => {
+    const users = new UserAPI(mockUsers)
+    users.addUser({})
     expect(true).toBeTruthy()
   })
 })
