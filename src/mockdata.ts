2bc143864255ecb1ab0f3be36a371271009d2144
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-05-20 09:37:22 -0700

    spacing

--- src/mockdata.ts
+++ src/mockdata.ts
@@ -5,6 +5,7 @@ const mockUsers = [
   { id: "2", name: "ron", age: 41, favoriteColor: "blue" },
   { id: "3", name: "tim", age: 27, favoriteColor: "red" },
 ]
+
 const userObject = { name: "minoka", age: 31, favoriteColor: "green" }
 
 const updatingUserObject = {
@@ -13,17 +14,20 @@ const updatingUserObject = {
   age: 100,
   favoriteColor: "red",
 }
+
 const updatingUserObjectError = {
   name: "minoka",
   age: 100,
   favoriteColor: "red",
 }
+
 const updatingUserObjectErrorWithId = {
   id: "124324614614331461346",
   name: "minoka",
   age: 100,
   favoriteColor: "red",
 }
+
 export {
   mockUsers,
   userObject,
