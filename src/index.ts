10ef2cb7e8f5b59dfbce670ed405d16aa9da9f78
Andy Plymate <andy.plymate@gmail.com>
2022-06-06 16:56:38 -0700

    create userapi skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-//Define class here
+class User {
+  name: string;
+  favColor: string;
+  age: number;
+
+    constructor(name: string, color: string, age: number) {
+    this.name = name;
+    this.favColor = color;
+    this.age = age;
+  }
+}
+
+class UserAPI {
+  users: Array<User>;
+
+  constructor() {
+    this.users = []
+  } 
+
+  assignId() {
+
+  }
+
+  addUser(user: User) {
+
+  }
+
+  getUserById(id: number) {
+
+  }
+
+  getUsers() {
+
+  }
+
+  deleteUserById(id: number) {
+
+  }
+
+  searchUserByName(name: string) {
+
+  }
+
+}
+
