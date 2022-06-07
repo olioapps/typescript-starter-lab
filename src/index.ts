25f63772463fa3d3e1ca23f1a4378e66a1509481
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-06-07 16:28:47 -0700

    restructure code to include try/catch blocks when calling class methods

--- src/index.ts
+++ src/index.ts
@@ -20,38 +20,28 @@ class UserAPI {
   }
 
   addUser(user: User) {
-    try {
-      if (!user.hasOwnProperty("id")) {
-        const existingUser = this.users.find(
-          x => x.name === user.name
-            && x.favoriteColor === user.favoriteColor
-            && x.age === user.age)
-        if (existingUser !== undefined) {
-          throw "ERROR: User with these properties already exists"
-        }
-        user.id = this.assignId()
-        this.users.push(user)
-        return user
-      } else {
-        throw "ERROR: Id incorrectly provided by input user"
+    if (!user.hasOwnProperty("id")) {
+      const existingUser = this.users.find(
+        x => x.name === user.name
+          && x.favoriteColor === user.favoriteColor
+          && x.age === user.age)
+      if (existingUser !== undefined) {
+        throw new Error("User with these properties already exists")
       }
-    }
-    catch (error) {
-      return error
+      user.id = this.assignId()
+      this.users.push(user)
+      return user
+    } else {
+      throw new Error("Id incorrectly provided by input user")
     }
   }
 
   getUserById(id: number) {
-    try {
-      const user = this.users.find(x => x.id === id)
-      if (user !== undefined) {
-        return user
-      } else {
-        throw "ERROR: User not found"
-      }
-    }
-    catch (error) {
-      return error
+    const user = this.users.find(x => x.id === id)
+    if (user !== undefined) {
+      return user
+    } else {
+      throw new Error("User was not found")
     }
   }
 
@@ -73,28 +63,43 @@ const x = new UserAPI()
 const user = {
   name: "Daniel",
   favoriteColor: "purple",
-  age: 33
+  age: 33,
 }
-console.log("Test: Should add a user object to UserAPI object x, and assign an Id of 1")
-console.log("Input:", user)
-x.addUser(user)
-console.log(`Describe: x.getUserById(1)`)
-console.log(`Expect: User {name: 'Daniel', favoriteColor: 'purple', age: 33, id: 1}`)
-console.log("Result:", x.getUserById(1))
-console.log("------------------------------------------")
+console.log(`Test:
+Should add a user object to UserAPI object x, and assign an Id of 1
+`)
+console.log(`Expect:
+{name: 'Daniel', favoriteColor: 'purple', age: 33, id: 1}
+`)
+console.log("Result:")
+try {
+  x.addUser(user)
+  console.log(user)
+} catch (e: any) {
+  console.log(e.message)
+}
+console.log("--------------------------------------------------------------------")
 
 const userTwo = {
   name: "Bob",
   favoriteColor: "green",
   age: 102
 }
-console.log("Test: Should add a user to UserAPI object x, and assign an Id of 2")
-console.log("Input:", userTwo)
-x.addUser(userTwo)
-console.log(`Describe: x.getUserById(2)`)
-console.log(`Expect: User {name: 'Bob', favoriteColor: 'green', age: 102, id: 2}`)
-console.log("Result:", x.getUserById(2))
-console.log("------------------------------------------")
+console.log(`Test:
+Should add a user to UserAPI object x, and assign an Id of 2
+`)
+console.log(`Expect:
+{name: 'Bob', favoriteColor: 'green', age: 102, id: 2}
+`)
+console.log("Result:")
+try {
+  x.addUser(userTwo)
+  console.log(userTwo)
+} catch (e: any) {
+  console.log(e.message)
+}
+console.log("--------------------------------------------------------------------")
+
 
 const userTwoDuplicateId = {
   name: "Steve",
@@ -102,28 +107,67 @@ const userTwoDuplicateId = {
   age: 17,
   id: 2
 }
-console.log("Test: Should fail as an Id is provided, Only objects without Ids present will be added")
-console.log("Input:", userTwoDuplicateId)
-console.log(`Describe: x.addUser(userTwoDuplicate)`)
-console.log(`Expect: "ERROR: Id incorrectly provided by input user"`)
-console.log("Result:", x.addUser(userTwoDuplicateId))
-console.log("------------------------------------------")
-
+console.log(`Test:
+Error when adding object that contains an id
+`)
+console.log(`Expect:
+Id incorrectly provided by input user
+`)
+console.log("Result:")
+try {
+  x.addUser(userTwoDuplicateId)
+  console.log(userTwoDuplicateId)
+} catch (e: any) {
+  console.log(e.message)
+}
+console.log("--------------------------------------------------------------------")
 
 const userTwoDuplicateUserProps = {
   name: "Bob",
   favoriteColor: "green",
   age: 102
 }
-console.log("Test: Should attempt to add a user with identical properties of userTwo, but fail as they match")
-console.log("Input:", userTwoDuplicateUserProps)
-console.log(`Describe: x.addUser(userTwoDuplicateUserProps)`)
-console.log(`Expect: "ERROR: User with these properties already exists"`)
-console.log("Result:", x.addUser(userTwoDuplicateUserProps))
-console.log("------------------------------------------")
 
-console.log("Test: Should attempt to locate a user by an Id that doesn't exist in the x object")
-console.log(`Describe: x.getUserById(9)`)
-console.log(`Expect: "ERROR: User not found"`)
-console.log("Result:", x.getUserById(9))
-console.log("------------------------------------------")
+console.log(`Test:
+Error when adding user with same properties as existing user
+`)
+console.log(`Expect:
+User with these properties already exists
+`)
+console.log("Result:")
+try {
+  x.addUser(userTwoDuplicateUserProps)
+  console.log(userTwoDuplicateUserProps)
+} catch (e: any) {
+  console.log(e.message)
+}
+console.log("--------------------------------------------------------------------")
+
+console.log(`Test:
+Should successfully find user based on id
+`)
+console.log(`Expect:
+{name: 'Bob', favoriteColor: 'green', age: 102, id: 2}
+`)
+console.log("Result")
+try {
+  console.log(x.getUserById(2))
+} catch (e: any) {
+  console.log(e.message)
+}
+console.log("--------------------------------------------------------------------")
+
+
+console.log(`Test:
+Error when locating a user by an id that doesn't exist in the x UserAPI class
+`)
+console.log(`Expect:
+User was not found
+`)
+console.log("Result")
+try {
+  console.log(x.getUserById(9))
+} catch (e: any) {
+  console.log(e.message)
+}
+console.log("--------------------------------------------------------------------")
