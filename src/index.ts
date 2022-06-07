377edf6141fd0468aa3c0e2660aeb2ac3d1b4fce
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-06-06 17:09:54 -0700

    add user and userAPI skeelton code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
 //Define class here
+class User {
+  name: string;
+  favoriteColor: string;
+  age: number;
+  id: number;
+  constructor(name: string, favoriteColor: string, age: number, id: number) {
+    this.name = name;
+    this.favoriteColor = favoriteColor;
+    this.age = age;
+    this.id = id;
+  }
+}
+
+class UserAPI {
+  users: Array<User>;
+
+  constructor() {
+    this.users = [];
+  }
+
+  assignId() {
+
+  }
+
+  addUser(user: User) {
+
+  }
+
+  getUserById(id: number) {
+
+  }
+
+  getUsers() {
+
+  }
+
+  deleteUserById(id: number) {
+
+  }
+
+  searchUserByName(name: string) {
+
+  }
+}
+
+// const x = new UserAPI();
+// const user = new User("Daniel", "purple", 33, 0);
+// x.addUser(user);
