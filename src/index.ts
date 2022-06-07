176413341609d1eb44a00e2af98a729f4a87ea42
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-06-07 14:00:58 -0700

    add tests and code to account for adding user with existing Id

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ interface User {
 }
 
 class UserAPI {
-  users: Array<User>
-  currentId: number
+  private users: Array<User>
+  private currentId: number
 
   constructor() {
     this.users = []
-    this.currentId = 0
+    this.currentId = 1
   }
 
   private assignId() {
@@ -20,16 +20,30 @@ class UserAPI {
   }
 
   addUser(user: User) {
-    user.id = this.assignId()
-    this.users.push(user)
+    try {
+      if (this.getUserById(Number(user.id)) === "ERROR: User not found") {
+        user.id = this.assignId()
+        this.users.push(user)
+        return user
+      } else {
+        throw "ERROR: A user with that Id already exists"
+      }
+    } catch (error) {
+      return error
+    }
   }
 
   getUserById(id: number) {
-    const user = this.users.find(x => x.id === id)
-    if (user !== undefined) {
-      return user
-    } else {
-      return "User not found"
+    try {
+      const user = this.users.find(x => x.id === id)
+      if (user !== undefined) {
+        return user
+      } else {
+        throw "ERROR: User not found"
+      }
+    }
+    catch (error) {
+      return error
     }
   }
 
@@ -46,25 +60,64 @@ class UserAPI {
   }
 }
 
-const x = new UserAPI();
+const x = new UserAPI()
 
 const user = {
   name: "Daniel",
   favoriteColor: "purple",
   age: 33
-};
+}
+console.log("Test: Should add a user object to UserAPI object x, and assign an Id of 1")
+console.log("Input:", user)
 x.addUser(user)
-console.log(`Describe(getUserById(0)) - Expect(User {'Daniel', 'purple', 33, 0})`)
-console.log("Result: ", x.getUserById(0))
+console.log(`Describe: x.getUserById(1)`)
+console.log(`Expect: User {name: 'Daniel', favoriteColor: 'purple', age: 33, id: 1}`)
+console.log("Result:", x.getUserById(1))
+console.log("------------------------------------------")
 
 const userTwo = {
   name: "Bob",
   favoriteColor: "green",
   age: 102
-};
-x.addUser(userTwo);
-console.log(`Describe(getUserById(1)) - Expect(User {'Bob', 'green', 102, 1})`)
-console.log("Result: ", x.getUserById(1))
+}
+console.log("Test: Should add a user to UserAPI object x, and assign an Id of 2")
+console.log("Input:", userTwo)
+x.addUser(userTwo)
+console.log(`Describe: x.getUserById(2)`)
+console.log(`Expect: User {name: 'Bob', favoriteColor: 'green', age: 102, id: 2}`)
+console.log("Result:", x.getUserById(2))
+console.log("------------------------------------------")
 
-console.log(`Describe(getUserById(2)) - Expect("User not found")`)
-console.log("Result: ", x.getUserById(2))
+const userTwoDuplicate = {
+  name: "Steve",
+  favoriteColor: "yellow",
+  age: 17,
+  id: 2
+}
+console.log("Test: Should attempt to add a user with an already existing Id of 2, but fail as a user with Id of 2 already exists")
+console.log("Input:", userTwoDuplicate)
+console.log(`Describe: x.addUser(userTwoDuplicate)`)
+console.log(`Expect: "ERROR: A user with that Id already exists"`)
+console.log("Result:", x.addUser(userTwoDuplicate))
+console.log("------------------------------------------")
+
+
+const userThree = {
+  name: "Jack",
+  favoriteColor: "orange",
+  age: 65,
+  id: 9
+}
+
+console.log("Test: Should add a user object that already has an Id of 9, but add the user with the next incremental Id")
+console.log("Input:", userThree)
+console.log(`Describe: x.addUser(userThree)`)
+console.log(`Expect: User {name: 'Jack', favoriteColor: 'orange', age: 65, id: 3}`)
+console.log("Result:", x.addUser(userThree))
+console.log("------------------------------------------")
+
+console.log("Test: Should attempt to locate a user by an Id that doesn't exist in the x object")
+console.log(`Describe: x.getUserById(9)`)
+console.log(`Expect: "ERROR: User not found"`)
+console.log("Result:", x.getUserById(9))
+console.log("------------------------------------------")
