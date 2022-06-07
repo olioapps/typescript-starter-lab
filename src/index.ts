555d3ccd5c06c617fb2b24a2bb74ca6a1b5c50a2
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-06-07 11:30:20 -0700

    remove semi colons

--- src/index.ts
+++ src/index.ts
@@ -15,21 +15,21 @@ class UserAPI {
   }
 
   private assignId() {
-    this.currentId++;
-    return this.currentId - 1;
+    this.currentId++
+    return this.currentId - 1
   }
 
   addUser(user: User) {
-    user.id = this.assignId();
-    this.users.push(user);
+    user.id = this.assignId()
+    this.users.push(user)
   }
 
   getUserById(id: number) {
-    const user = this.users.find(x => x.id === id);
+    const user = this.users.find(x => x.id === id)
     if (user !== undefined) {
-      return user;
+      return user
     } else {
-      return "User not found";
+      return "User not found"
     }
   }
 
@@ -53,9 +53,9 @@ const user = {
   favoriteColor: "purple",
   age: 33
 };
-x.addUser(user);
-console.log(`Describe(getUserById(0)) - Expect(User {'Daniel', 'purple', 33, 0})`);
-console.log("Result: ", x.getUserById(0));
+x.addUser(user)
+console.log(`Describe(getUserById(0)) - Expect(User {'Daniel', 'purple', 33, 0})`)
+console.log("Result: ", x.getUserById(0))
 
 const userTwo = {
   name: "Bob",
@@ -63,8 +63,8 @@ const userTwo = {
   age: 102
 };
 x.addUser(userTwo);
-console.log(`Describe(getUserById(1)) - Expect(User {'Bob', 'green', 102, 1})`);
-console.log("Result: ", x.getUserById(1));
+console.log(`Describe(getUserById(1)) - Expect(User {'Bob', 'green', 102, 1})`)
+console.log("Result: ", x.getUserById(1))
 
-console.log(`Describe(getUserById(2)) - Expect("User not found")`);
-console.log("Result: ", x.getUserById(2));
+console.log(`Describe(getUserById(2)) - Expect("User not found")`)
+console.log("Result: ", x.getUserById(2))
