93ba18073b1db0d23bc736776b93fcc7714d9187
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-06-07 16:00:37 -0700

    mock data written out

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,16 @@
-//write tests here
+import {
+  firstMockEventStream,
+  secondMockEventStream,
+  thirdMockEvenStream,
+  fourthMockEventStream
+} from './mockEventStream'
 
 describe('Tests will go here!', () => {
   it('should pass', () => {
     expect(true).toBeTruthy()
+  })
+
+  it('should fail', () => {
+    console.log('firstMockEventStream', firstMockEventStream)
   })
 })
---
+++ src/mockEventStream.ts
@@ -0,0 +1,196 @@
+export const firstMockEventStream = [
+	{
+		timestamp: 123123123,
+		eventType: "new message",
+	},
+	{
+		timestamp: 123123124,
+		eventType: "new message",
+	},
+	{
+		timestamp: 123123125,
+		eventType: "new message",
+	},
+	{
+		timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+		timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+		timestamp: 123123125,
+		eventType: "screenshot",
+	},
+	{
+		timestamp: 123123125,
+		eventType: "screenshot",
+	},
+	{
+		timestamp: 123123125,
+		eventType: "message",
+	},
+	{
+		timestamp: 123123125,
+		eventType: "message",
+    },
+]
+export const secondMockEventStream = [
+    {
+        timestamp: 123123123,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123124,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "screenshot",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "screenshot",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "message",
+    },
+    {
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+]
+export const thirdMockEvenStream = [
+    {
+        timestamp: 123123125,
+		eventType: "screenshot",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "screenshot",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "message",
+    },
+    {
+        timestamp: 123123123,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123124,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "screenshot",
+	},
+] 
+export const fourthMockEventStream = [
+    {
+        timestamp: 123123124,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "message",
+    },
+    {
+        timestamp: 123123124,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+    },
+    {
+        timestamp: 123123124,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+    },
+    {
+        timestamp: 123123124,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "new message",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+	{
+        timestamp: 123123125,
+		eventType: "view",
+	},
+]
