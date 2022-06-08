4c7dc001975bd1d3eddbf1479b6f75c7d93c59fd
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-06-08 15:47:41 -0700

    updateing test for more robust descriptions

--- src/index.test.ts
+++ src/index.test.ts
@@ -10,40 +10,33 @@ import { getHighestScoringRegion, findRegionScore, scoreEvent } from "./index"
 
 
 describe('Tests will go here!', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy()
-  })
-
-  it('should return the correct array region', () => {
-    const highestScoringRegion = getHighestScoringRegion(firstMockEventStream)
-    const expected = [
+  it('should return the first highest scoring region array when there is a tie', () => {
+    const firstHighestScoringRegion = getHighestScoringRegion(firstMockEventStream)
+    const firstExpected = [
       { timestamp: 123123125, eventType: 'new message' },
       { timestamp: 123123125, eventType: 'view' },
       { timestamp: 123123125, eventType: 'view' },
       { timestamp: 123123125, eventType: 'screenshot' },
       { timestamp: 123123125, eventType: 'screenshot' },
     ]
-    expect(highestScoringRegion).toEqual(expected)
-  })
-
-  it('should return the correct array region', () => {
-    const highestScoringRegion = getHighestScoringRegion(secondMockEventStream)
-    const expected = [
-      { timestamp: 123123125, eventType: 'view' },
-      { timestamp: 123123125, eventType: 'screenshot' },
-      { timestamp: 123123125, eventType: 'screenshot' },
-    ]
-    expect(highestScoringRegion).toEqual(expected)
-  })
-  
-  it('should return the correct array region', () => {
-    const highestScoringRegion = getHighestScoringRegion(thirdMockEventStream)
-    const expected = [
+    expect(firstHighestScoringRegion).toEqual(firstExpected)
+    const secondHighestScoringRegion = getHighestScoringRegion(thirdMockEventStream)
+    const secondExpected = [
       { timestamp: 123123125, eventType: 'screenshot' },
       { timestamp: 123123125, eventType: 'screenshot' },
       { timestamp: 123123125, eventType: 'new message' },
       { timestamp: 123123125, eventType: 'new message' },
       { timestamp: 123123123, eventType: 'new message' },
+    ]
+    expect(secondHighestScoringRegion).toEqual(secondExpected)
+  })
+
+  it('should return the correct array regions if there is less then 5 in event stream', () => {
+    const highestScoringRegion = getHighestScoringRegion(secondMockEventStream)
+    const expected = [
+      { timestamp: 123123125, eventType: 'view' },
+      { timestamp: 123123125, eventType: 'screenshot' },
+      { timestamp: 123123125, eventType: 'screenshot' },
     ]
     expect(highestScoringRegion).toEqual(expected)
   })
@@ -69,7 +62,7 @@ describe('Tests will go here!', () => {
     const regionScore = findRegionScore(secondMockRegion)
     expect(regionScore).toEqual(9)
   })
-  
+
   it('should give the correct score for each region', () => {
     const regionScore = findRegionScore(
       [
