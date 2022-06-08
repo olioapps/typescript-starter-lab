5ff359c1aea5d50e2855a218f17f0a0106a11250
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-06-08 16:21:36 -0700

    updateing/ upgrading  testing suit structure

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,75 +8,84 @@ import {
 } from './mockEventStream'
 import { getHighestScoringRegion, findRegionScore, scoreEvent } from "./index"
 
-describe('testing all functions for getting the highest scoring region of event stream', () => {
-
-  it('should return the first highest scoring region array when there is a tie', () => {
-    const firstHighestScoringRegion = getHighestScoringRegion(firstMockEventStream)
-    const firstExpected = [
-      { timestamp: 123123125, eventType: 'new message' },
-      { timestamp: 123123125, eventType: 'view' },
-      { timestamp: 123123125, eventType: 'view' },
-      { timestamp: 123123125, eventType: 'screenshot' },
-      { timestamp: 123123125, eventType: 'screenshot' },
-    ]
-    expect(firstHighestScoringRegion).toEqual(firstExpected)
-
-    const secondHighestScoringRegion = getHighestScoringRegion(thirdMockEventStream)
-    const secondExpected = [
-      { timestamp: 123123125, eventType: 'screenshot' },
-      { timestamp: 123123125, eventType: 'screenshot' },
-      { timestamp: 123123125, eventType: 'new message' },
-      { timestamp: 123123125, eventType: 'new message' },
-      { timestamp: 123123123, eventType: 'new message' },
-    ]
-    expect(secondHighestScoringRegion).toEqual(secondExpected)
-  })
-
-  it('should return the correct array regions if there is less then 5 in event stream', () => {
-    const highestScoringRegion = getHighestScoringRegion(secondMockEventStream)
-    const expected = [
-      { timestamp: 123123125, eventType: 'view' },
-      { timestamp: 123123125, eventType: 'screenshot' },
-      { timestamp: 123123125, eventType: 'screenshot' },
-    ]
-    expect(highestScoringRegion).toEqual(expected)
-  })
+describe('Event scoring function tests.', () => {
   
-  it('should return the correct array region', () => {
-    const highestScoringRegion = getHighestScoringRegion(fourthMockEventStream)
-    const expected = [
-      { timestamp: 123123125, eventType: 'view' },
-      { timestamp: 123123124, eventType: 'screenshot' },
-      { timestamp: 123123125, eventType: 'new message' },
-      { timestamp: 123123125, eventType: 'view' },
-      { timestamp: 123123125, eventType: 'screenshot' },
-    ]
-    expect(highestScoringRegion).toEqual(expected)
-  })
+  describe('Tests for getting the highest scoring region in a event stream', () => {
 
-  it('should give the correct score for each region', () => {
-    const firstRegionScore = findRegionScore(firstMockRegion)
-    expect(firstRegionScore).toEqual(8)
+    it('should return the first highest scoring region array when there is a tie', () => {
+      const firstHighestScoringRegion = getHighestScoringRegion(firstMockEventStream)
+      const firstExpected = [
+        { timestamp: 123123125, eventType: 'new message' },
+        { timestamp: 123123125, eventType: 'view' },
+        { timestamp: 123123125, eventType: 'view' },
+        { timestamp: 123123125, eventType: 'screenshot' },
+        { timestamp: 123123125, eventType: 'screenshot' },
+      ]
+      expect(firstHighestScoringRegion).toEqual(firstExpected)
+      
+      const secondHighestScoringRegion = getHighestScoringRegion(thirdMockEventStream)
+      const secondExpected = [
+        { timestamp: 123123125, eventType: 'screenshot' },
+        { timestamp: 123123125, eventType: 'screenshot' },
+        { timestamp: 123123125, eventType: 'new message' },
+        { timestamp: 123123125, eventType: 'new message' },
+        { timestamp: 123123123, eventType: 'new message' },
+      ]
+      expect(secondHighestScoringRegion).toEqual(secondExpected)
+    })
+    
+    it('should return the correct array regions if there is less then 5 in event stream', () => {
+      const highestScoringRegion = getHighestScoringRegion(secondMockEventStream)
+      const expected = [
+        { timestamp: 123123125, eventType: 'view' },
+        { timestamp: 123123125, eventType: 'screenshot' },
+        { timestamp: 123123125, eventType: 'screenshot' },
+      ]
+      expect(highestScoringRegion).toEqual(expected)
+    })
 
-    const secondRegionScore = findRegionScore(secondMockRegion)
-    expect(secondRegionScore).toEqual(9)
-  })
-
-  it('should give the correct score for each region even if there is less then 5', () => {
-    const thirdRegionScore = findRegionScore(
-      [
+    it('should return the correct array region', () => {
+      const highestScoringRegion = getHighestScoringRegion(fourthMockEventStream)
+      const expected = [
+        { timestamp: 123123125, eventType: 'view' },
+        { timestamp: 123123124, eventType: 'screenshot' },
         { timestamp: 123123125, eventType: 'new message' },
         { timestamp: 123123125, eventType: 'view' },
         { timestamp: 123123125, eventType: 'screenshot' },
-      ])
-    expect(thirdRegionScore).toEqual(6)
+      ]
+      expect(highestScoringRegion).toEqual(expected)
+    })
   })
 
-  it('return the correct score value from each event', () => {
-    expect(scoreEvent({ timestamp: 123123125, eventType: 'view' })).toEqual(2)
+  describe('Tests for getting the total score of each region', () => {
 
-    expect(scoreEvent({ timestamp: 123123125, eventType: 'screenshot' })).toEqual(3)
+    it('should give the correct score for each region', () => {
+      const firstRegionScore = findRegionScore(firstMockRegion)
+      expect(firstRegionScore).toEqual(8)
+      
+      const secondRegionScore = findRegionScore(secondMockRegion)
+      expect(secondRegionScore).toEqual(9)
+    })
     
-    expect(scoreEvent({ timestamp: 123123125, eventType: 'new message' })).toEqual(1)
+    it('should give the correct score for each region even if there is less then 5', () => {
+      const thirdRegionScore = findRegionScore(
+        [
+          { timestamp: 123123125, eventType: 'new message' },
+          { timestamp: 123123125, eventType: 'view' },
+          { timestamp: 123123125, eventType: 'screenshot' },
+        ])
+        expect(thirdRegionScore).toEqual(6)
+    })
+  })
+  
+  describe('Test for getting the correct score for each event', () => {
+    
+    it('return the correct score value from each event', () => {
+      expect(scoreEvent({ timestamp: 123123125, eventType: 'view' })).toEqual(2)
+      
+      expect(scoreEvent({ timestamp: 123123125, eventType: 'screenshot' })).toEqual(3)
+      
+      expect(scoreEvent({ timestamp: 123123125, eventType: 'new message' })).toEqual(1)
+    })
   })
 })
