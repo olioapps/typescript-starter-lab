377cdfcebaed600e5fb087ddfb2bde7eb12b1aa1
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-06-08 11:51:43 -0700

    algorithim is working all test are passing. took me while.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 //Define class/functions here
-import { firstMockEventStream } from "./mockEventStream"
+
 export interface Event {
   timestamp: number
-  eventType: string
+  eventType: 'screenshot' | 'view' | 'new message'
 }
 
 const findScore = (eventStream: Array<Event>): number => {
@@ -14,18 +14,20 @@ const findScore = (eventStream: Array<Event>): number => {
     }
     return acc + 1
   }, 0)
-  
+
   return score
 }
 
 export const getHighestScoringRegion = (eventStream: Array<Event>): Array<Event> => {
-  let potentialArray = eventStream.slice(0, 5)
-  const score = findScore(potentialArray)
-  console.log('score', score)
   
-  // then need to loop through each array and check the next five and determine their score
-    // if the score is higher then the potentailArray score then replace potentail array
-  return eventStream
+  const eventStreamScore: number[]= eventStream.reduce((acc, next, index): any => {
+    const eventRegion = eventStream.slice(index, index + 5)
+    return [...acc, findScore(eventRegion) ] 
+  }, [])
+
+  const max: number = Math.max(...eventStreamScore)
+  const index = eventStreamScore.indexOf(max as number)
+
+  return eventStream.slice(index, index + 5)
 }
 
-console.log(getHighestScoringRegion(firstMockEventStream))
