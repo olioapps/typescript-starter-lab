e028cd3c105185020d66088780ffba2d2e26c3c6
Andy Plymate <andy.plymate@gmail.com>
2022-06-08 10:14:47 -0700

    funtionality for getUsers, deleteUserById, searchUserByName, searchUserByFavoriteColor.

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,22 @@ interface IUser {
 }
 
 class UserAPI {
-  private users: Array<IUser>
-  private nextId: number
+  private _users: Array<IUser>
+  private _nextId: number
 
   constructor() {
-    this.users = []
-    this.nextId = 0
+    this._users = []
+    this._nextId = 0
   } 
 
   private assignId() {
-    this.nextId++
-    return this.nextId
+    this._nextId++
+    return this._nextId
+  }
+  
+  private createUser(name: string, favColor: string, age: number): IUser {
+    const id = this.assignId()
+    return { id, name, favColor, age }
   }
 
   addUser(user: IUser) {
@@ -24,42 +29,64 @@ class UserAPI {
       throw new SyntaxError("Please resubmit without pre-exisitng ID field.")
     } 
 
-    const result = (this.users).find( ( { name, favColor, age }: IUser) => (name === user.name && favColor === user.favColor && age === user.age))
+    const result = (this._users).find( ( { name, favColor, age }: IUser) => (name === user.name && favColor === user.favColor && age === user.age))
     if (result) {
       throw new Error("A user with those properties already exists in the database.")
     } 
 
     const { name, favColor, age } = user
     const newUser = this.createUser(name, favColor, age)
-    this.users.push(newUser)
+    this._users.push(newUser)
     return newUser
   }
 
   getUserById(id: number) {
-    const foundUser = this.users.find( user => user.id === id) 
+    const foundUser = this._users.find( user => user.id === id) 
     if (foundUser) {
       return foundUser
     }
-    throw new ReferenceError("No user found.")
+    throw new ReferenceError(`No user found with id ${id}.`)
   }
 
-  getUsers() {
-
+  get_Users() {
+    if (this._users.length > 0) {
+      return this._users
+    }
+    throw new Error(`No users to return`)
   }
 
   deleteUserById(id: number) {
-
+    const indexOfUser = this._users.findIndex( user => user.id === id) 
+    if (indexOfUser > -1) {
+      this._users.splice(indexOfUser,1)
+      return `Deleted user id ${id}`
+    } else{
+      throw new ReferenceError(`No user found with id ${id}.`)
+    }  
   }
 
   searchUserByName(name: string) {
+    const users = this._users.filter( user => user.name.toLowerCase() === name.toLowerCase())
+    
+    if (users.length > 0) {
+      return users
+    } 
+    throw new ReferenceError(`No user found with name ${name}`)
+  }
 
+  search_UsersByFavoriteColor(color: string) {
+    const users = this._users.filter( user => user.favColor.toLowerCase() === color.toLowerCase())
+    if (users.length > 0) {
+      return users
+    } 
+    throw new ReferenceError(`No users favorite color is ${color}`)
   }
   
-  private createUser(name: string, favColor: string, age: number): IUser {
-    const id = this.assignId()
-    return { id, name, favColor, age }
-  }
 }
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
 
 const x = new UserAPI()
 const user = {
@@ -67,12 +94,22 @@ const user = {
   favColor: "blue",
   age: 247
 }
+const user2 = {
+  name: "Andy",
+  favColor: "Green",
+  age: 200
+} 
+const user3 = {
+  name: "Sarah",
+  favColor: "Blue",
+  age: 200
+} 
 const userDuplicate = {
   name: "andy",
   favColor: "blue",
   age: 247
 }
-const user2 = {
+const userWithId = {
   id: 5,
   name: "andy",
   favColor: "blue",
@@ -80,27 +117,91 @@ const user2 = {
 }
 
 try {
-  console.log("ADD:", x.addUser(user))
+  console.log("GETALL:", x.get_Users())
 } catch (error: any) {
-  console.log(error.name,":", error.message)}
+  console.log("GETALL:", error.name,":", error.message)
+}
 
 try {
-  console.log("ADD:", x.addUser(userDuplicate))
+  console.log("ADD:", x.addUser(user))
 } catch (error: any) {
-  console.log(error.name,":", error.message)}
+  console.log("ADD:", error.name,":", error.message)
+}
 
 try {
   console.log("ADD:", x.addUser(user2))
 } catch (error: any) {
-  console.log(error.name,":", error.message)}
+  console.log("ADD:", error.name,":", error.message)
+}
+
+try {
+  console.log("ADD:", x.addUser(user3))
+} catch (error: any) {
+  console.log("ADD:", error.name,":", error.message)
+}
+
+try {
+  console.log("ADD:", x.addUser(userDuplicate))
+} catch (error: any) {
+  console.log("ADD:", error.name,":", error.message)
+}
+
+try {
+  console.log("ADD:", x.addUser(userWithId))
+} catch (error: any) {
+  console.log("ADD:", error.name,":", error.message)
+}
 
 try {
   console.log("GET:", x.getUserById(1))
 } catch (error: any) {
-  console.log(error.name,":", error.message)}
+  console.log("GET:", error.name,":", error.message)
+}
 
 try {
-  console.log("GET:", x.getUserById(2))
+  console.log("GET:", x.getUserById(100))
 } catch (error: any) {
-  console.log(error.name,":", error.message)
+  console.log("GET:", error.name,":", error.message)
+}
+
+try {
+  console.log("GETALL:", x.get_Users())
+} catch (error: any) {
+  console.log("GETALL:", error.name,":", error.message)
+}
+
+try {
+  console.log("SEARCH:", x.searchUserByName("Andy"))
+} catch (error: any) {
+  console.log("SEARCH:", error.name,":", error.message)
+}
+
+try {
+  console.log("SEARCH:", x.searchUserByName("Jessica"))
+} catch (error: any) {
+  console.log("SEARCH:", error.name,":", error.message)
+}
+
+try {
+  console.log("SEARCH COLOR:", x.search_UsersByFavoriteColor("Blue"))
+} catch (error: any) {
+  console.log("SEARCH COLOR:", error.name,":", error.message)
+}
+
+try {
+  console.log("SEARCH COLOR:", x.search_UsersByFavoriteColor("Pink"))
+} catch (error: any) {
+  console.log("SEARCH COLOR:", error.name,":", error.message)
+}
+
+try {
+  console.log("DELETE:", x.deleteUserById(1))
+} catch (error: any) {
+  console.log("DELETE:", error.name,":", error.message)
+}
+
+try {
+  console.log("DELETE:", x.deleteUserById(1))
+} catch (error: any) {
+  console.log("DELETE:", error.name,":", error.message)
 }
