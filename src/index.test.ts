4670b0cfdc9e62de872e8ab04adcfff70604afa5
Andy Plymate <andy.plymate@gmail.com>
2022-06-09 17:09:59 -0700

    all tests pass after converting dataset to object

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,50 +2,60 @@ import { UserAPI, IUser } from './index'
 
 describe(UserAPI, () => {
   it('check that UserAPI is initialized', () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-      }]
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      }
+    }
     const x = new UserAPI(seedUsers)
     expect(x).toBeInstanceOf(UserAPI)
   })
   it("getUsers() returns intitial seed data", () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-      }]
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      }
+    }
     const x = new UserAPI(seedUsers)
     expect(x.getUsers()).toEqual(seedUsers)
   })
   
   it("addUser() correctly adds a user with a random id", () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      }
+    }
     const user2 = {
       name: "Andy",
       favColor: "Green",
       age: 200
     }
     const x = new UserAPI(seedUsers)
-    const newUser = x.addUser(user2)
-    expect(x.getUsers()).toEqual([seedUsers[0], newUser])
+    const newUser: any= x.addUser(user2)
+    let newUserState: any = {...seedUsers}
+    newUserState[newUser.id] = newUser
+    expect(x.getUsers()).toEqual(newUserState)
   })
 
   it("addUser() throws error when duplicating user info", () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      }
+    }
     const userDuplicate = {
       name: "andy",
       favColor: "blue",
@@ -56,12 +66,14 @@ describe(UserAPI, () => {
   })
 
   it("addUser() throws error supplied with pre-existing user.id", () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      }
+    }
     const userWithId = {
       id: "5",
       name: "andy",
@@ -73,50 +85,53 @@ describe(UserAPI, () => {
   })
 
   it('getUserById() returns expected user', () => {
-    const seedUsers: Array<IUser> = [    
-      { 
-      id: "1", 
+    const seedUsers: object = {    
+      "1": { 
+        id: "1", 
         name: "andy", 
         favColor: "blue", 
         age: 247 
       },
-      { 
+      "2": { 
         id: "2", 
           name: "Dan", 
           favColor: "blue", 
           age: 150 
       }
-    ]
+    }
     const x = new UserAPI(seedUsers)
-    expect(x.getUserById("2")).toEqual(seedUsers[1])
+    
+    expect(x.getUserById("2")).toEqual({id: "2", name: "Dan", favColor: "blue", age: 150 })
   })
 
   it('getUserById() throws error if user with id is not found', () => {
-    const seedUsers: Array<IUser> = [    
-      { 
-      id: "1", 
+    const seedUsers: object = {    
+      "1": { 
+        id: "1", 
         name: "andy", 
         favColor: "blue", 
         age: 247 
       },
-      { 
+      "2": { 
         id: "2", 
           name: "Dan", 
           favColor: "blue", 
           age: 150 
       }
-    ]
+    }
     const x = new UserAPI(seedUsers)
     expect( () => x.getUserById("100")).toThrow(ReferenceError)
   })
 
   it('getUsers() returns multiple users', () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      }
+    }
     const user2 = {
       name: "Andy",
       favColor: "Green",
@@ -128,48 +143,57 @@ describe(UserAPI, () => {
       age: 200
     } 
     const x = new UserAPI(seedUsers)
-    const newUser2 = x.addUser(user2)
-    const newUser3 = x.addUser(user3)
-
-    expect(x.getUsers()).toEqual([seedUsers[0], newUser2, newUser3])
+    const newUser2: any = x.addUser(user2)
+    const newUser3: any = x.addUser(user3)
+    let newUserState: any = {...seedUsers}
+    newUserState[newUser2.id] = newUser2
+    newUserState[newUser3.id] = newUser3
+    expect(x.getUsers()).toEqual(newUserState)
   })
 
   it('deleteUserById() deletes user with given id', () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
-    const user2 = {
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      },
+      "2": {  
+      id: "2",  
       name: "Andy",
       favColor: "Green",
       age: 200
+      }
     } 
     const x = new UserAPI(seedUsers)
-    const newUser2 = x.addUser(user2)
     x.deleteUserById("1")
-    expect(x.getUsers()).toEqual([newUser2])
+    expect(x.getUsers()).toEqual([
+      {id: "2", name: "Andy",favColor: "Green",  age: 200}
+    ])
   })
   it('deleteUserById() throws error if user is not found', () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
-
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      }
+    }
     const x = new UserAPI(seedUsers)
     expect( () => {x.deleteUserById("100")}).toThrow(ReferenceError)
   })
 
   it('searchUserByName() returns all instances of given name, independant of capitals', () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      }
+    }
     const user2 = {
       name: "Andy",
       favColor: "Green",
@@ -179,31 +203,37 @@ describe(UserAPI, () => {
       name: "Sarah",
       favColor: "Blue",
       age: 200
-    } 
+    }
     const x = new UserAPI(seedUsers)
-    const newUser2 = x.addUser(user2)
+    const newUser2: any = x.addUser(user2)
     x.addUser(user3)
-    expect(x.searchUserByName("Andy")).toEqual([seedUsers[0], newUser2])
+    let newUserState: any = {...seedUsers}
+    newUserState[newUser2.id] = newUser2
+    expect(x.searchUserByName("Andy")).toEqual(Object.values(newUserState))
   })
   it('searchUserByName() throws error if no user is found', () => {
-    const seedUsers: Array<IUser> = [{ 
-      id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
+const seedUsers: object = {
+  "1": { 
+  id: "1", 
+  name: "andy", 
+  favColor: "blue", 
+  age: 247
+  }
+}
     const x = new UserAPI(seedUsers)
 
     expect( () => x.searchUserByName("Jessica")).toThrow(ReferenceError)
   })
 
   it('searchUserByFavoriteColor() returns all instances users with given fav color, independant of capitals', () => {
-    const seedUsers: Array<IUser> = [{ 
+    const seedUsers: object = {
+      "1": { 
       id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
+      name: "andy", 
+      favColor: "blue", 
+      age: 247
+      }
+    }
     const user2 = {
       name: "Andy",
       favColor: "Green",
@@ -216,16 +246,22 @@ describe(UserAPI, () => {
     } 
     const x = new UserAPI(seedUsers)
     x.addUser(user2)
-    const newUser3 = x.addUser(user3)
-    expect(x.searchUsersByFavoriteColor("blue")).toEqual([seedUsers[0], newUser3])
+    const newUser3: any = x.addUser(user3)
+    // expect(x.searchUsersByFavoriteColor("blue")).toEqual({...seedUsers, newUser3)
+
+    let newUserState: any = {...seedUsers}
+    newUserState[newUser3.id] = newUser3
+    expect(x.searchUsersByFavoriteColor("blue")).toEqual(Object.values(newUserState))
   })
   it('searchUserByFavoriteColor() throws error if no user is found with given fav color', () => {
-    const seedUsers: Array<IUser> = [{ 
-      id: "1", 
-        name: "andy", 
-        favColor: "blue", 
-        age: 247 
-    }]
+const seedUsers: object = {
+  "1": { 
+  id: "1", 
+  name: "andy", 
+  favColor: "blue", 
+  age: 247
+  }
+}
     const x = new UserAPI(seedUsers)
 
     expect( () => x.searchUsersByFavoriteColor("Pink")).toThrow(ReferenceError)
--- src/index.ts
+++ src/index.ts
@@ -6,31 +6,39 @@ export interface IUser {
 }
 
 export class UserAPI {
-  private _users: Array<IUser>
+  private _users: object
 
-  constructor(seedData: Array<IUser> = []) {
-    this._users = [...seedData]
+  constructor(seedData: object = {}) {
+    this._users = {...seedData}
   } 
 
-  addUser(user: IUser) {
-    if (user.id) {
+  addUser(userToAdd: IUser) {
+    if (userToAdd.id) {
       throw new SyntaxError("Please resubmit without pre-exisitng ID field.")
     } 
+    let user = {...userToAdd}
+    const result = (Object.values(this._users)).filter( existingUser => (
+          existingUser.name.toLowerCase() === user.name.toLowerCase() && 
+          existingUser.favColor.toLowerCase() === user.favColor.toLowerCase() && 
+          existingUser.age === user.age
+        )
+
+    )
     
-    user.id = Date.now().toString()
-    const result = (this._users).find( ( { name, favColor, age }: IUser) => (name === user.name && favColor === user.favColor && age === user.age))
-    
-    if (result) {
-      throw new Error("A user with those properties already exists in the database.")
-    } 
-    
-    this._users = [...this._users, user]
+    if (result.length) {
+        throw new Error("A user with those properties already exists in the database.")
+      } 
+    const id = Date.now().toString() + Math.floor(Math.random()*100)
+    user.id = id
+    let newUsersState: any = {...this._users}
+    newUsersState[id] = user
+
+    this._users = {...newUsersState}
     return user
   }
 
   getUserById(id: string) {
-    const foundUser = this._users.find( user => user.id === id) 
-    
+    const foundUser = Object.values(this._users).find( user => user.id === id) 
     if (foundUser) {
       return foundUser
     }
@@ -45,10 +53,10 @@ export class UserAPI {
   }
 
   deleteUserById(id: string) {
-    const user = this._users.filter( user => user.id === id)
+    const user = Object.values(this._users).filter( user => user.id === id)
     
     if (user.length) {
-      const newUserState = this._users.filter( user => user.id != id)
+      const newUserState = Object.values(this._users).filter( user => user.id != id)
       this._users = [...newUserState]
       return user
     } else{
@@ -57,7 +65,7 @@ export class UserAPI {
   }
 
   searchUserByName(name: string) {
-    const users = this._users.filter( user => user.name.toLowerCase() === name.toLowerCase())
+    const users = Object.values(this._users).filter( user => user.name.toLowerCase() === name.toLowerCase())
     
     if (users.length) {
       return users
@@ -66,7 +74,7 @@ export class UserAPI {
   }
 
   searchUsersByFavoriteColor(color: string) {
-    const users = this._users.filter( user => user.favColor.toLowerCase() === color.toLowerCase())
+    const users = Object.values(this._users).filter( user => user.favColor.toLowerCase() === color.toLowerCase())
     if (users.length) {
       return users
     } 
@@ -74,3 +82,49 @@ export class UserAPI {
   }
   
 }
+
+// const seedUsers: object = {    
+//   "1": { 
+//     id: "1", 
+//     name: "andy", 
+//     favColor: "blue", 
+//     age: 247 
+//   },
+//   "2": { 
+//     id: "2", 
+//       name: "Dan", 
+//       favColor: "blue", 
+//       age: 150 
+//   }
+// }
+// const x = new UserAPI(seedUsers)
+
+
+// const user2 = {
+//   name: "Andy",
+//   favColor: "Green",
+//   age: 200
+// } 
+
+// const user3 = {
+//   name: "Sarah",
+//   favColor: "Blue",
+//   age: 200
+// } 
+
+
+// console.log("ADD USER2", x.addUser(user2))
+// // console.log("GET USERS #2",x.getUsers());
+// console.log("ADD USER3", x.addUser(user3))
+// console.log("GET USERS #3",x.getUsers());
+// try {
+//   console.log("ADD USER3", x.addUser(user3))
+// } catch (error) {
+//   console.log(error);
+// }
+// console.log("GET USERS #3",x.getUsers());
+
+
+
+
+
