fba03b238401f26e9f56db5c100cc50a8697d2ed
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-06-10 15:46:38 -0700

    convert jest tests to use TypeScript

--- src/index.test.ts
+++ src/index.test.ts
@@ -11,8 +11,8 @@ describe('UserAPI() objects and method ', () => {
 
     it("should return empty object", () => {
       const users = new UserAPI()
-      const actual = users.getUsers()
-      const expected = new Array
+      const actual: Array<IUser> = users.getUsers()
+      const expected: Array<IUser> = new Array
       expect(expected).toEqual(actual)
     })
 
@@ -21,8 +21,8 @@ describe('UserAPI() objects and method ', () => {
         ["1"]: { name: 'Daniel', favoriteColor: 'purple', age: 33, id: "1" },
         ["2"]: { name: 'Jenny', favoriteColor: 'yellow', age: 129, id: "2" }
       })
-      const actual = users.getUsers()
-      const expected = [
+      const actual: Array<IUser> = users.getUsers()
+      const expected: Array<IUser> = [
         { name: 'Daniel', favoriteColor: 'purple', age: 33, id: "1" },
         { name: 'Jenny', favoriteColor: 'yellow', age: 129, id: "2" }
       ]
@@ -34,8 +34,8 @@ describe('UserAPI() objects and method ', () => {
 
     it("should return empty array of objects", () => {
       const users = new UserAPI()
-      const actual = users.getUsers()
-      const expected = new Array
+      const actual: Array<IUser> = users.getUsers()
+      const expected: Array<IUser> = new Array
       expect(expected).toEqual(actual)
     })
 
@@ -44,8 +44,8 @@ describe('UserAPI() objects and method ', () => {
         ["1"]: { name: 'Daniel', favoriteColor: 'purple', age: 33, id: "1" },
         ["2"]: { name: 'Jenny', favoriteColor: 'yellow', age: 129, id: "2" }
       })
-      const actual = users.getUsers()
-      const expected = [
+      const actual: Array<IUser> = users.getUsers()
+      const expected: Array<IUser> = [
         { name: 'Daniel', favoriteColor: 'purple', age: 33, id: "1" },
         { name: 'Jenny', favoriteColor: 'yellow', age: 129, id: "2" }
       ]
@@ -56,30 +56,30 @@ describe('UserAPI() objects and method ', () => {
   describe('UserAPI.addUser()', () => {
 
     const users = new UserAPI()
-    let user = <IUser>{}
+    let user: IUser = <IUser>{}
 
     it("should successfully add and return new user", () => {
       user = users.addUser({ name: 'Daniel', favoriteColor: 'purple', age: 33 })
-      const actual = user
-      const expected = { name: 'Daniel', favoriteColor: 'purple', age: 33, id: user.id }
+      const actual: IUser = user
+      const expected: IUser = { name: 'Daniel', favoriteColor: 'purple', age: 33, id: user.id }
       expect(expected).toEqual(actual)
     })
 
     it("should return the updated list of users", () => {
-      const actual = users.getUsers()
-      const expected = [user]
+      const actual: Array<IUser> = users.getUsers()
+      const expected: Array<IUser> = [user]
       expect(expected).toEqual(actual)
     })
 
     it("should fail to add user if Id is provided", () => {
-      const userWithId = { name: 'Daniel', favoriteColor: 'green', age: 33, id: "2" }
-      const actual = () => { users.addUser(userWithId) }
+      const userWithId: IUser = { name: 'Daniel', favoriteColor: 'green', age: 33, id: "2" }
+      const actual: {} = () => { users.addUser(userWithId) }
       expect(actual).toThrow("Id incorrectly provided by input user")
     })
 
     it("should fail to add user with same props", () => {
-      const userDuplicate = { name: 'Daniel', favoriteColor: 'purple', age: 33 }
-      const actual = () => { users.addUser(userDuplicate) }
+      const userDuplicate: IUser = { name: 'Daniel', favoriteColor: 'purple', age: 33 }
+      const actual: {} = () => { users.addUser(userDuplicate) }
       expect(actual).toThrow("User with these properties already exists")
     })
   })
@@ -89,13 +89,13 @@ describe('UserAPI() objects and method ', () => {
     const users = new UserAPI({ ["1"]: { name: 'Daniel', favoriteColor: 'purple', age: 33, id: "1" } })
 
     it("should return a user", () => {
-      const actual = users.getUserById("1")
-      const expected = { name: 'Daniel', favoriteColor: 'purple', age: 33, id: "1" }
+      const actual: IUser = users.getUserById("1")
+      const expected: IUser = { name: 'Daniel', favoriteColor: 'purple', age: 33, id: "1" }
       expect(expected).toEqual(actual)
     })
 
     it("should fail to find user with non-existent Id", () => {
-      const actual = () => { users.getUserById("3") }
+      const actual: {} = () => { users.getUserById("3") }
       expect(actual).toThrow("User was not found")
     })
   })
@@ -109,14 +109,14 @@ describe('UserAPI() objects and method ', () => {
     })
 
     it("should delete then return deleted user", () => {
-      const actual = users.deleteUserById('1')
-      const expected = { name: 'Larry', favoriteColor: 'gray', age: 544, id: "1" }
+      const actual: IUser = users.deleteUserById('1')
+      const expected: IUser = { name: 'Larry', favoriteColor: 'gray', age: 544, id: "1" }
       expect(expected).toEqual(actual)
     })
 
     it("should return an array of remaining users", () => {
-      const actual = users.getUsers()
-      const expected = [
+      const actual: Array<IUser> = users.getUsers()
+      const expected: Array<IUser> = [
         { name: 'Daniel', favoriteColor: 'purple', age: 33, id: "2" },
         { name: 'Jenny', favoriteColor: 'yellow', age: 88, id: "3" }
       ]
@@ -124,7 +124,7 @@ describe('UserAPI() objects and method ', () => {
     })
 
     it("should fail to find user with non-existent Id", () => {
-      const actual = () => { users.deleteUserById("7") }
+      const actual: {} = () => { users.deleteUserById("7") }
       expect(actual).toThrow("User was not found")
     })
   })
@@ -138,8 +138,8 @@ describe('UserAPI() objects and method ', () => {
     })
 
     it("should return array of two users", () => {
-      const actual = users.searchUserByName("DaNIeL")
-      const expected = [
+      const actual: Array<IUser> = users.searchUserByName("DaNIeL")
+      const expected: Array<IUser> = [
         { name: 'Daniel', favoriteColor: 'gray', age: 544, id: "1" },
         { name: 'Daniel', favoriteColor: 'purple', age: 33, id: "2" }
       ]
@@ -147,7 +147,7 @@ describe('UserAPI() objects and method ', () => {
     })
 
     it("should fail to find user", () => {
-      const actual = () => { users.searchUserByName("Billy") }
+      const actual: {} = () => { users.searchUserByName("Billy") }
       expect(actual).toThrow("User(s) not found")
     })
   })
@@ -161,8 +161,8 @@ describe('UserAPI() objects and method ', () => {
     })
 
     it("should find all favorite color users", () => {
-      const actual = users.searchUsersByFavoriteColor("YeLlOw")
-      const expected = [
+      const actual: Array<IUser> = users.searchUsersByFavoriteColor("YeLlOw")
+      const expected: Array<IUser> = [
         { name: 'Daniel', favoriteColor: 'yellow', age: 544, id: "1" },
         { name: 'Jenny', favoriteColor: 'yellow', age: 88, id: "3" },
       ]
@@ -170,7 +170,7 @@ describe('UserAPI() objects and method ', () => {
     })
 
     it("should fail to find any users", () => {
-      const actual = () => { users.searchUsersByFavoriteColor("Orange") }
+      const actual: {} = () => { users.searchUsersByFavoriteColor("Orange") }
       expect(actual).toThrow("User(s) not found")
     })
   })
