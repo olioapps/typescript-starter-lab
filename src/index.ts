63805498d019e32ab224fd9a70aa443158807ae9
Andy Plymate <andy.plymate@gmail.com>
2022-06-13 15:04:45 -0700

    assign points to events in stream correctly

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,66 @@
-//Define class/functions here
+interface IEvent {
+  timestamp: number,
+  eventType: eventTypeState,
+	value?: number
+}
+
+type eventTypeState = 'newMessage' | 'screenShot' | 'view'
+
+function eventStream(stream: Array<IEvent>) {
+  if(stream.length <= 5 ) { return stream }
+
+	const newStream =  [...stream ]
+	newStream.forEach( e => e.value = assignPointValue(e.eventType));
+
+	return newStream
+}
+
+function assignPointValue(eventType: eventTypeState) {
+	switch (eventType) {
+		case 'newMessage':
+			return 1
+		case 'view':
+			return 2
+		case 'screenShot':
+			return 3
+		default:
+			throw new Error("Not a valid eventType")
+	}
+}
+
+const seed: Array<IEvent> = [
+  {
+  timestamp: 123123123,
+	eventType: 'newMessage'
+	},
+	{
+		timestamp: 123123124,
+		eventType: 'newMessage'
+	},
+	{
+		timestamp: 123123125,
+		eventType: 'newMessage'
+	},
+	{
+		timestamp: 123123125,
+		eventType: 'view'
+	},
+	{
+		timestamp: 123123125,
+		eventType: 'view'
+	},
+	{
+		timestamp: 123123125,
+		eventType: 'screenShot'
+	},
+	{
+		timestamp: 123123125,
+		eventType: 'screenShot'
+	},
+	{
+		timestamp: 123123125,
+		eventType: 'newMessage'
+	}
+]
+
+console.log('eventstream call:', eventStream(seed))
