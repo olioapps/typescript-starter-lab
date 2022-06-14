13bb761f6f6f5eb64043107844762179e97983b5
Andy Plymate <andy.plymate@gmail.com>
2022-06-14 16:08:19 -0700

    prettier extension to change formatting

--- src/index.ts
+++ src/index.ts
@@ -6,40 +6,42 @@ export interface IEvent {
 export type eventTypeState = 'newMessage' | 'screenShot' | 'view'
 
 export function eventStream(stream: Array<IEvent>): ReadonlyArray<IEvent> {
-  if(stream.length <= 5 ) { return stream }
+  if (stream.length <= 5) return stream
 
-	const newStream: Array<IEvent> = stream.map( event => ({...event, value: assignPointValue(event.eventType)}))
+  const newStream: Array<IEvent> = stream.map((event) => ({
+    ...event,
+    value: assignPointValue(event.eventType),
+  }))
   const index: number = findLastIndexOfRegionWithHighestSum(newStream)
-	const region: ReadonlyArray<IEvent> = stream.splice((index-4), 5) 
-	return region
+  const region: ReadonlyArray<IEvent> = stream.splice(index - 4, 5)
+  return region
 }
 
 export function assignPointValue(eventType: eventTypeState): Readonly<number> {
-	switch (eventType) {
-		case 'newMessage':
-			return 1
-		case 'view':
-			return 2
-		case 'screenShot':
-			return 3
-		default:
-			throw new Error("Not a valid eventType")
-	}
+  switch (eventType) {
+    case 'newMessage':
+      return 1
+    case 'view':
+      return 2
+    case 'screenShot':
+      return 3
+    default:
+      throw new Error('Not a valid eventType')
+  }
 }
 
 export function findLastIndexOfRegionWithHighestSum(stream: Array<IEvent>): number {
-  if(stream.length <= 5 ) { return stream.length-1 }
+  if (stream.length <= 5) return stream.length - 1
 
   let index = 4
   let maxCumulativeSum = 0
-  for(let i=4; i<stream.length; i++) {
-    const currentSum = (
-      stream[i].value! + 
-      stream[i-1].value! + 
-      stream[i-2].value! + 
-      stream[i-3].value! + 
-      stream[i-4].value!
-    )
+  for (let i = 4; i < stream.length; i++) {
+    const currentSum =
+      stream[i].value! +
+      stream[i - 1].value! +
+      stream[i - 2].value! +
+      stream[i - 3].value! +
+      stream[i - 4].value!
     if (currentSum > maxCumulativeSum) {
       maxCumulativeSum = currentSum
       index = i
