a39cb9e0dd96f59af12c58cf2686c5cdde3a03f7
Andy Plymate <andy.plymate@gmail.com>
2022-06-14 10:52:06 -0700

    add overall describe block to test file and pretty line spacing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,39 +1,43 @@
 import { testSeed1, testSeed2, testSeed2Expected, testSeed3, testSeed3Expected } from './mockTestData'
 import { IEvent, eventStream, assignPointValue } from '../src/index'
 
-describe('eventStream()', () => {
+describe('event puzzler solution', () => {
 
-  it('return the given stream if there are 5 events or less', () => {
-    const actual: ReadonlyArray<IEvent> = eventStream(testSeed1)
-    expect(actual).toEqual(testSeed1)
+  describe('eventStream()', () => {
+    
+    it('return the given stream if there are 5 events or less', () => {
+      const actual: ReadonlyArray<IEvent> = eventStream(testSeed1)
+      expect(actual).toEqual(testSeed1)
+    })
+
+    it('returns the region with with the highest cumulative point value from the input', () => {
+      const actual: ReadonlyArray<IEvent> = eventStream(testSeed2)
+      const expected: ReadonlyArray<IEvent> = testSeed2Expected
+      expect(actual).toEqual(expected)
+    })
+    
+    it('should return first region in stream when cumlative point value ties another region', () => {
+      const actual: ReadonlyArray<IEvent> = eventStream(testSeed3)
+      const expected: ReadonlyArray<IEvent> = testSeed3Expected
+      expect(actual).toEqual(expected)
+    })
   })
 
-  it('returns the region with with the highest cumulative point value from the input', () => {
-    const actual: ReadonlyArray<IEvent> = eventStream(testSeed2)
-    const expected: ReadonlyArray<IEvent> = testSeed2Expected
-    expect(actual).toEqual(expected)
-  })
-  
-  it('should return first region in stream when cumlative point value ties another region', () => {
-    const actual: ReadonlyArray<IEvent> = eventStream(testSeed3)
-    const expected: ReadonlyArray<IEvent> = testSeed3Expected
-    expect(actual).toEqual(expected)
+  describe('assignpointValue()', () => {
+    
+    it('return 1 if passed "newMessage"', () => {
+      const actual: number = assignPointValue('newMessage')
+      expect(actual).toEqual(1)
+    })
+
+    it('return 2 if passed "view"', () => {
+      const actual: number = assignPointValue('view')
+      expect(actual).toEqual(2)
+    })
+
+    it('return 3 if passed "screenShot"', () => {
+      const actual: number = assignPointValue('screenShot')
+      expect(actual).toEqual(3)
+    })
   })
 })
-
-describe('assignpointValue()', () => {
-
-  it('return 1 if passed "newMessage"', () => {
-    const actual: number = assignPointValue('newMessage')
-    expect(actual).toEqual(1)
-  })
-  it('return 2 if passed "view"', () => {
-    const actual: number = assignPointValue('view')
-    expect(actual).toEqual(2)
-  })
-  it('return 3 if passed "screenShot"', () => {
-    const actual: number = assignPointValue('screenShot')
-    expect(actual).toEqual(3)
-  })
-})
-
