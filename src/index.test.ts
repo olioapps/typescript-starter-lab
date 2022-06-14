efa26ea839fcdc8bbe54c039e16bd036b79d4216
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-06-14 14:26:05 -0700

    update code to remove unnecessary functions

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,8 @@
 //write tests here
-import { EventStream, EventInput, Region } from './index'
+import { EventStream, Event, Region } from './index'
 
 describe('Class EventStream', () => {
-  const seedData: ReadonlyArray<EventInput> = [
+  const seedData: ReadonlyArray<Event> = [
     {
       timestamp: 123123123,
       eventType: "newMessage"
@@ -40,7 +40,7 @@ describe('Class EventStream', () => {
       eventType: "newMessage"
     }
   ]
-  const largerSeedData: ReadonlyArray<EventInput> = [
+  const largerSeedData: ReadonlyArray<Event> = [
     {
       timestamp: 123123123,
       eventType: "newMessage"
@@ -120,95 +120,11 @@ describe('Class EventStream', () => {
       const eventStream = new EventStream([...seedData])
       expect(eventStream).toBeInstanceOf(EventStream)
     })
-
-    it("should confirm events get added to eventStream object", () => {
-      const eventStream = new EventStream([...seedData])
-      const actual: ReadonlyArray<EventInput> = eventStream.getUnsortedEvents()
-      const expected: ReadonlyArray<EventInput> = seedData
-      expect(expected).toEqual(actual)
-    })
-
-    it("should fail to create EventStream with wrong input data type", () => {
-      // const eventStream = new EventStream({...seedData})
-      const badEventStream: {} = () => new EventStream({ ...seedData })
-      expect(badEventStream).toThrow("Input of object was found. Please provide a valid array.")
-    })
-
-    it("should fail with not enough obj provided", () => {
-      const badSeedData: ReadonlyArray<EventInput> = [
-        {
-          timestamp: 123123123,
-          eventType: "newMessage"
-        },
-        {
-          timestamp: 123123124,
-          eventType: "newMessage"
-        },
-        {
-          timestamp: 123123125,
-          eventType: "newMessage"
-        },
-        {
-          timestamp: 123123125,
-          eventType: "newMessage"
-        }
-      ]
-      const badEventStream: {} = () => new EventStream([...badSeedData])
-      expect(badEventStream).toThrow("Not enough events to score. Please provide at least five")
-    })
-
-    it("should fail with if invalid event type provided", () => {
-      const badSeedData: ReadonlyArray<EventInput> = [
-        {
-          timestamp: 123123123,
-          eventType: "newMessage"
-        },
-        {
-          timestamp: 123123124,
-          eventType: "newMessage"
-        },
-        {
-          timestamp: 123123125,
-          eventType: "newMessage"
-        },
-        {
-          timestamp: 123123125,
-          eventType: "view"
-        },
-        {
-          timestamp: 123123125,
-          eventType: "porcupine"
-        },
-        {
-          timestamp: 123123125,
-          eventType: "screenshot"
-        },
-        {
-          timestamp: 123123125,
-          eventType: "newMessage"
-        },
-        {
-          timestamp: 123123125,
-          eventType: "newMessage"
-        }
-      ]
-      const badEventStream: {} = () => new EventStream([...badSeedData])
-      expect(badEventStream).toThrow(`Invalid event type found (event: 4, timestamp: 123123125, type: porcupine)`)
-    })
-  })
-
-  describe('getUnsortedScores()', () => {
-    it("should confirm events get added to eventStream object", () => {
-      const eventStream = new EventStream(seedData)
-      const actual: ReadonlyArray<EventInput> = eventStream.getUnsortedEvents()
-      const expected: ReadonlyArray<EventInput> = seedData
-      expect(expected).toEqual(actual)
-    })
   })
 
   describe('getHighestScoringRegion()', () => {
     it("should return the highest scoring region from Seed Data", () => {
-      const expected: ReadonlyArray<EventInput> = [
+      const expected: ReadonlyArray<Event> = [
         {
           timestamp: 123123125,
           eventType: "newMessage"
@@ -231,12 +147,12 @@ describe('Class EventStream', () => {
         }
       ]
       const eventStream = new EventStream(seedData)
-      const actual: ReadonlyArray<EventInput> = eventStream.getHighestScoringRegion()
+      const actual: ReadonlyArray<Event> = eventStream.getHighestScoringRegion()
       expect(expected).toEqual(actual)
     })
 
     it("should return the highest scoring region from larger set of Seed Data", () => {
-      const expected: ReadonlyArray<EventInput> = [
+      const expected: ReadonlyArray<Event> = [
         {
           timestamp: 123123126,
           eventType: "view"
@@ -259,44 +175,27 @@ describe('Class EventStream', () => {
         }
       ]
       const eventStream = new EventStream(largerSeedData)
-      const actual: Array<EventInput> = eventStream.getHighestScoringRegion()
-      expect(expected).toEqual(actual)
-    })
-  })
-
-  describe('getSortedScores()', () => {
-    it("should return seed data regions sorted by score", () => {
-      const expected: ReadonlyArray<Region> = [
-        { regionId: 2, score: 11, inputLocations: [2, 3, 4, 5, 6] },
-        { regionId: 3, score: 11, inputLocations: [3, 4, 5, 6, 7] },
-        { regionId: 4, score: 10, inputLocations: [4, 5, 6, 7, 8] },
-        { regionId: 1, score: 9, inputLocations: [1, 2, 3, 4, 5] },
-        { regionId: 0, score: 7, inputLocations: [0, 1, 2, 3, 4] }
-      ]
-      const eventStream = new EventStream(seedData)
-      const actual: ReadonlyArray<Region> = eventStream.getSortedScores()
+      const actual: Array<Event> = eventStream.getHighestScoringRegion()
       expect(expected).toEqual(actual)
     })
 
-    it("should return larger seed data regions sorted by score", () => {
-      const expected: ReadonlyArray<Region> = [
-        { regionId: 13, score: 14, inputLocations: [13, 14, 15, 16, 17] },
-        { regionId: 12, score: 13, inputLocations: [12, 13, 14, 15, 16] },
-        { regionId: 2, score: 11, inputLocations: [2, 3, 4, 5, 6] },
-        { regionId: 3, score: 11, inputLocations: [3, 4, 5, 6, 7] },
-        { regionId: 11, score: 11, inputLocations: [11, 12, 13, 14, 15] },
-        { regionId: 4, score: 10, inputLocations: [4, 5, 6, 7, 8] },
-        { regionId: 5, score: 10, inputLocations: [5, 6, 7, 8, 9] },
-        { regionId: 10, score: 10, inputLocations: [10, 11, 12, 13, 14] },
-        { regionId: 1, score: 9, inputLocations: [1, 2, 3, 4, 5] },
-        { regionId: 6, score: 9, inputLocations: [6, 7, 8, 9, 10] },
-        { regionId: 9, score: 9, inputLocations: [9, 10, 11, 12, 13] },
-        { regionId: 8, score: 8, inputLocations: [8, 9, 10, 11, 12] },
-        { regionId: 0, score: 7, inputLocations: [0, 1, 2, 3, 4] },
-        { regionId: 7, score: 7, inputLocations: [7, 8, 9, 10, 11] }
+    it("should return seed data when count <= 5", () => {
+      const expected: ReadonlyArray<Event> = [
+        {
+          timestamp: 123123126,
+          eventType: "view"
+        },
+        {
+          timestamp: 123123126,
+          eventType: "screenshot"
+        },
+        {
+          timestamp: 123123127,
+          eventType: "screenshot"
+        }
       ]
-      const eventStream = new EventStream(largerSeedData)
-      const actual: ReadonlyArray<Region> = eventStream.getSortedScores()
+      const eventStream = new EventStream(expected)
+      const actual: Array<Event> = eventStream.getHighestScoringRegion()
       expect(expected).toEqual(actual)
     })
   })
--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,50 @@
-export interface EventInput {
+export interface Event {
   timestamp: Readonly<number>,
-  eventType: Readonly<string>
+  eventType: Readonly<"newMessage" | "view" | "screenshot">
 }
 
-export interface Region {
-  regionId: Readonly<number>,
-  score: Readonly<number>,
-  inputLocations: ReadonlyArray<number>
+interface Region {
+  regionStartId: Readonly<number>,
+  score: Readonly<number>
 }
 
 export class EventStream {
 
-  private _scoreTable: Record<Readonly<string>, Readonly<number>> = {
-    ["newMessage"]: 1,
-    ["view"]: 2,
-    ["screenshot"]: 3
+  private _scoreTable = {
+    "newMessage": 1,
+    "view": 2,
+    "screenshot": 3
   }
 
-  private _sortedWinners: ReadonlyArray<Region> = []
+  private _winningRegion: Region = { regionStartId: 0, score: 0 }
 
-  constructor(private _data: ReadonlyArray<EventInput>) {
-    if (!Array.isArray(_data)) {
-      throw `Input of ${typeof _data} was found. Please provide a valid array.`
-    } else {
-      _data = [..._data]
-    }
+  constructor(private _inputData: ReadonlyArray<Event>) {
     this._setSortedWinners()
   }
 
-  getUnsortedEvents(): ReadonlyArray<EventInput> {
-    return [...this._data]
-  }
-
   private _setSortedWinners() {
-    const numOfRegions: number = 1 + (this._data.length - 5)
-    if (numOfRegions < 1) {
-      throw "Not enough events to score. Please provide at least five"
-    }
-    let regionObjs: Record<Readonly<number>, Readonly<Region>> = {}
-    for (let x = 0; x < numOfRegions; x++) {
-      regionObjs[x] = this._calculateRegion(x)
-    }
-    this._sortedWinners = Object.values(regionObjs).sort(function (a, b) {
-      return b.score - a.score
-    })
-  }
-
-  private _calculateRegion(regionNum: number): Region {
-    let regionScore: number = 0
-    let regionNumbers: Array<number> = []
-    for (let y = regionNum; y < regionNum + 5; y++) {
-      const eventType: string = this._data[y].eventType
-      if (!this._scoreTable[eventType]) {
-        throw `Invalid event type found (event: ${y}, timestamp: ${this._data[y].timestamp}, type: ${eventType})`
+    const numOfRegions = 1 + (this._inputData.length - 5)
+    if (numOfRegions > 1) {
+      let regionObjs: Array<Region> = []
+      for (let x = 0; x < numOfRegions; x++) {
+        let newRegion: Region = {
+          regionStartId: x,
+          score: 0
+        }
+        for (let y = x; y < x + 5; y++) {
+          newRegion.score += this._scoreTable[this._inputData[y].eventType]
+        }
+        regionObjs.push(newRegion)
       }
-      regionNumbers.push(y)
-      regionScore += this._scoreTable[eventType]
-    }
-    return {
-      regionId: regionNum,
-      score: regionScore,
-      inputLocations: [...regionNumbers]
+      this._winningRegion = {
+        ...(regionObjs.sort((a, b) => {
+          return b.score - a.score
+        }))[0]
+      }
     }
   }
 
-  getSortedScores(): ReadonlyArray<Region> {
-    return [...this._sortedWinners]
-  }
-
-  getHighestScoringRegion(): Array<EventInput> {
-    let highestScoringRegion: Array<EventInput> = []
-    this._sortedWinners[0].inputLocations.forEach((eventInRegion: number) => {
-      highestScoringRegion.push([...this._data][eventInRegion])
-    })
-    return [...highestScoringRegion]
+  getHighestScoringRegion(): Array<Event> {
+    return [...this._inputData.slice(this._winningRegion.regionStartId, this._winningRegion.regionStartId + 5)]
   }
 }
