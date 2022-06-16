bd8da366e9ae7c78e46be61d3fbea21a43227145
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-06-16 14:36:32 -0700

    convert functions into encapsulated variables

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export interface Event {
-  timestamp: Readonly<number>,
+  readonly timestamp: number,
   eventType: Readonly<"newMessage" | "view" | "screenshot">
 }
 
@@ -8,17 +8,21 @@ interface Region {
   score: Readonly<number>
 }
 
-export function getWinningRegion(inputData: ReadonlyArray<Event>): ReadonlyArray<Event> {
+const scoreEvent = (ev: Event): number => {
   const scoreTable = {
     "newMessage": 1,
     "view": 2,
     "screenshot": 3
   }
+  return scoreTable[ev.eventType]
+}
+
+export const getWinningRegion = (inputData: ReadonlyArray<Event>): ReadonlyArray<Event> => {
   const numOfRegions = 1 + (inputData.length - 5)
   if (numOfRegions > 1) {
     const regionObjs: Array<Region> = inputData.map((ev, index) => {
       const score: number = [...inputData.slice(index, index + 5)].reduce((runningScore, currentEvent) =>
-        runningScore + scoreTable[currentEvent.eventType], 0
+        runningScore + scoreEvent(currentEvent), 0
       )
       return { regionStartId: index, score: score }
     })
