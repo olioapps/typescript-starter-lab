32c26f51c3ed93b6ed19c290533a1696bd4f52d4
Todd Aden <todd.aden@olio.com>
2023-08-14 15:52:10 -0700

    add tests and unique function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,67 @@
-//write tests here
+import { Pets, getUniquePetTypes } from ".";
 
-describe('Tests will go here!', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy()
-  })
-})
+describe("Unique pet Types", () => {
+  it("returns empty object", () => {
+    // Arrange
+    const EMPTY_PET_LIST: ReadonlyArray<Pets> = [];
+    const expected = {};
+
+    // Act
+    const actual = getUniquePetTypes(EMPTY_PET_LIST);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns unique pet list objects", () => {
+    // Arrange
+    const PET_LIST: ReadonlyArray<Partial<Pets>> = [
+      {
+        type: "dog",
+      },
+      {
+        type: "dog",
+      },
+      {
+        type: "cat",
+      },
+    ];
+
+    const expected = {
+      dog: 2,
+      cat: 1,
+    };
+
+    // Act
+    const actual = getUniquePetTypes(PET_LIST as ReadonlyArray<Pets>);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns unique pet list objects case insensitive", () => {
+    // Arrange
+    const PET_LIST: ReadonlyArray<Partial<Pets>> = [
+      {
+        type: "DOG",
+      },
+      {
+        type: "dOg",
+      },
+      {
+        type: "Cat",
+      },
+    ];
+
+    const expected = {
+      dog: 2,
+      cat: 1,
+    };
+
+    // Act
+    const actual = getUniquePetTypes(PET_LIST as ReadonlyArray<Pets>);
+
+    // Assert
+    expect(actual).toEqual(expected);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
 //Define class here
+
+export interface Pets {
+  readonly name: string;
+  readonly type: string;
+  readonly dangerLevel: number;
+}
+
+export const getUniquePetTypes = (pets: ReadonlyArray<Pets>) => {
+  const container: Record<string, number> = {};
+  for (const pet of pets) {
+    const key = pet.type.toLocaleLowerCase();
+    container[key] = container[key] == undefined ? 1 : container[key] + 1;
+  }
+  return container;
+};
+
+// DONE - Function that counts number of unique pet types
+// DONE - Function is case insensitive
+// DONE - Returns all items lower case
+// DONE - Returns an Object containing the result
+// DONE - { "dog": 4, "cat": 2, "locust": 1 }
+// DONE - returns empty object if empty
+
+// Function that counts number of unique pet types
+// Function is case insensitive
+// Returns an Object containing the result
+// { "dog": 4, "cat": 2, "locust": 1 }
+// returns empty array if empty
+
+// Return the most dangerous animal
+// returns the full animal object
+
+// (animals, threshold) => bool
+// returns a bool if all pets are not dangerous
+// function takes a threshold as a secondary options
+
+// returns sorts by category then by name case insensitive
