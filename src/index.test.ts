f03b4510fb888dcc0e6debb26e37e07ca556adca
Emma Gerigscott <elgerig@gmail.com>
2023-08-14 16:39:34 -0700

    write two tests for first spread op func

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,29 @@
-//write tests here
+describe('Lesson One: Manipulating an array of objects', () => {
+  it('should return an empty array when an empty array is passed', () => {
+    // ARRANGE
+    const EMPTY_ARRAY = [];
+    const expected = [];
+    // ACT
+    const result = arrayCopier(EMPTY_ARRAY);
+    // ASSERT
+    expect(result).toBe(expected)
+  });
 
-describe('Tests will go here!', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy()
-  })
+  it('should return a copy of the array', () => {
+    // ARRANGE
+    const PEOPLE_LIST = [
+      { name: "Aron", age: 42 }, 
+      { name: "Stormi", age: 24 }
+    ];
+    const expected = [
+      { name: "Aron", age: 42 }, 
+      { name: "Stormi", age: 24 }
+    ];
+    // ACT
+    const result = arrayCopier(PEOPLE_LIST);
+    // ASSERT
+    expect(result).toEqual(expected);
+  });
+
+  it('')
 })
