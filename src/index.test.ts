7b251e36de46da31ea931b14314b26d905ae5022
Emma Gerigscott <elgerig@gmail.com>
2023-08-15 15:24:14 -0700

    red phase for test 1 & 2

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,47 @@
-//write tests here
+import { howdyMaker, carMaker } from ".";
 
-describe('Tests will go here!', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy()
-  })
-})
+describe('howdyMaker', () => {
+  it('should create a single object with keys defined by variables', () => {
+    // ARRANGE
+    let i = 0;
+    const expected = {
+      howdy1: 1,
+      howdy2: 2,
+      howdy3: 3
+    }
+    // ACT
+    const actual = howdyMaker(i);
+
+    // ASSERT
+    expect(actual).toEqual(expected)
+  });
+});
+
+describe('carMaker', () => {
+  it('should return new car object with new property names', () => {
+    // ARRANGE
+    const CAR_OBJECT = {
+      car_make: 'VW',
+      car_model: 'Thing',
+      car_value: 450
+    };
+    const expected = {
+      carMake: 'VW',
+      carModel: 'Thing',
+      carValue: 450
+    };
+
+    // ACT
+    const actual = carMaker(CAR_OBJECT)
+
+    // ASSERT
+    expect(actual).toEqual(expected);
+  });
+});
+
+
+// 1. Computed properties --> single object w/ keys defined as variables
+// 2. Computed properties, Destructuring --> function takes an object { make, model, value } and returns new object with new property names
+// 3. Computed properties, destructuring --> add new user to database, make the users id the key, function(newUserData, userDatabase) => return userDatabase with newUserData (immutable)
+// 4. Function (userRepository) => return sorted array of user objects based on sort key
+// 5. 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-//Define class here
+interface Car {
+  car_make: string,
+  car_model: string,
+  car_value: number
+}
+
+interface NewCar {
+  carMake: string,
+  carModel: string,
+  carValue: number
+}
+
+export const howdyMaker =(num: number) => {
+
+}
+
+export const carMaker = (car: Car) => {
+
+}
