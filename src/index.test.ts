90cc22a095e608d1976cc15fdc6d9c486c891731
Emma Gerigscott <elgerig@gmail.com>
2023-08-15 08:58:28 -0700

    add tests for age adder and remover

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,29 +1,67 @@
-describe('Lesson One: Manipulating an array of objects', () => {
-  it('should return an empty array when an empty array is passed', () => {
+interface People {
+  readonly name: string;
+  readonly age: number;
+}
+
+describe("arrayCopier", () => {
+  it("should return an empty array when an empty array is passed", () => {
     // ARRANGE
-    const EMPTY_ARRAY = [];
-    const expected = [];
+    const EMPTY_ARRAY: ReadonlyArray<People> = [];
+    const expected: ReadonlyArray<People> = [];
     // ACT
     const result = arrayCopier(EMPTY_ARRAY);
     // ASSERT
-    expect(result).toBe(expected)
+    expect(result).toBe(expected);
   });
 
-  it('should return a copy of the array', () => {
+  it("should return a copy of the array", () => {
     // ARRANGE
-    const PEOPLE_LIST = [
-      { name: "Aron", age: 42 }, 
-      { name: "Stormi", age: 24 }
+    const PEOPLE_LIST: ReadonlyArray<People> = [
+      { name: "Aron", age: 42 },
+      { name: "Stormi", age: 24 },
     ];
     const expected = [
-      { name: "Aron", age: 42 }, 
-      { name: "Stormi", age: 24 }
+      { name: "Aron", age: 42 },
+      { name: "Stormi", age: 24 },
     ];
     // ACT
     const result = arrayCopier(PEOPLE_LIST);
     // ASSERT
     expect(result).toEqual(expected);
   });
+});
 
-  it('')
-})
+describe("ageAdder", () => {
+  it("should add age to each person in agelessPeople array", () => {
+    // ARRANGE
+    const AGELESSPEOPLE_LIST: ReadonlyArray<Partial<People>> = [
+      { name: "Aron" },
+      { name: "Stormi" },
+    ];
+    const AGES_TO_ADD_LIST = [35, 98];
+    const expected = [
+      { name: "Aron", age: 35 },
+      { name: "Stormi", age: 98 },
+    ];
+    //
+    // ACT
+    const result = ageAdder(AGELESSPEOPLE_LIST, AGES_TO_ADD_LIST);
+    expect(result).toEqual(expected)
+  });
+});
+
+describe("ageRemover", () => {
+  it("should remove the age key and value from an array of people with ages", () => {
+    // ARRANGE
+    const PEOPLE_LIST: ReadonlyArray<People> = [
+      { name: "Aron", age: 42 },
+      { name: "Stormi", age: 24 },
+    ];
+    const expected: ReadonlyArray<Partial<People>> = [
+      { name: "Aron" },
+      { name: "Stormi" },
+    ];
+    // ACT
+    const result
+  });
+});
