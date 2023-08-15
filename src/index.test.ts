eb629ad1c16d70e6fdb4a0234b8aeb95e87b931b
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-15 14:53:37 -0700

    WIP: in the middle of writing test for #1

---
+++ src/index.test.ts
@@ -0,0 +1,11 @@
+//write tests here
+
+describe('computed properties', () => {
+  it('should return an object whose keys are defined by variables', () => {
+
+    //Arrange
+    const expected = 
+
+    expect(true).toBeTruthy()
+  })
+})
