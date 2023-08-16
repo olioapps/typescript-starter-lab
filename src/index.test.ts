b9f14402382c7df30b9ba89f38c7ccef7cb5c4f0
Emma Gerigscott <elgerig@gmail.com>
2023-08-16 10:04:16 -0700

    fix test notations + declare missing types

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,7 +4,7 @@ describe("arrayCopier", () => {
   it("should return an empty array when an empty array is passed", () => {
     // ARRANGE
     const EMPTY_ARRAY: ReadonlyArray<People> = [];
-    const expected: ReadonlyArray<Partial<People>> = [];
+    const expected: ReadonlyArray<People> = [];
     // ACT
     const result = arrayCopier(EMPTY_ARRAY);
     // ASSERT
@@ -17,7 +17,7 @@ describe("arrayCopier", () => {
       { name: "Aron", age: 42 },
       { name: "Stormi", age: 24 },
     ];
-    const expected = [
+    const expected: ReadonlyArray<People> = [
       { name: "Aron", age: 42 },
       { name: "Stormi", age: 24 },
     ];
@@ -40,7 +40,6 @@ describe("ageAdder", () => {
       { name: "Aron", age: 35 },
       { name: "Stormi", age: 98 },
     ];
-    //
     // ACT
     const result = ageAdder(AGELESSPEOPLE_LIST, AGES_TO_ADD_LIST);
     expect(result).toEqual(expected);
@@ -60,12 +59,14 @@ describe("ageRemover", () => {
     ];
     // ACT
     const result = ageRemover(PEOPLE_LIST);
+    // ASSERT
     expect(result).toEqual(expected);
   });
 });
 
 describe("ageRemover2", () => {
   it("should remove the age key from an array of people with ages", () => {
+    // ARRANGE
     const PEOPLE_LIST: ReadonlyArray<People> = [
       { name: "Aron", age: 42 },
       { name: "Stormi", age: 24 },
@@ -74,27 +75,33 @@ describe("ageRemover2", () => {
       { name: "Aron" },
       { name: "Stormi" },
     ];
+    // ACT
     const result = ageRemover2(PEOPLE_LIST);
+    // ASSERT
     expect(result).toEqual(expected);
   });
 });
 
 describe('ageRemover3', () => {
   it("should return an array of objects with only string name", () => {
+    // ARRANGE
     const PEOPLE_LIST: ReadonlyArray<People> = [
       { name: "Aron", age: 42 },
       { name: "Stormi", age: 24 },
     ];
-    const expected = [
+    const expected: ReadonlyArray<Partial<People>> = [
       { name: "Aron" }, { name: "Stormi" }
     ];
+    // ACT
     const result = ageRemover3(PEOPLE_LIST);
+    // ASSERT
     expect(result).toEqual(expected);
   });
 });
 
 describe('idAsName', () => {
   it('should return an array of objects with id instead of name key', () => {
+    // ARRANGE
     const PEOPLE_LIST: ReadonlyArray<People> = [
       { name: "Aron", age: 42 },
       { name: "Stormi", age: 24 },
@@ -103,28 +110,25 @@ describe('idAsName', () => {
       { id: "Aron", age: 42 },
       { id: "Stormi", age: 24 },
     ];
+    // ACT
     const result = idAsName(PEOPLE_LIST);
+    // ASSERT
     expect(result).toEqual(expected);
   });
 });
 
 describe('justNames', () => {
   it('should return an array of names', () => {
+    // ARRANGE
     const PEOPLE_LIST: ReadonlyArray<People> = [
       { name: "Aron", age: 42 },
       { name: "Stormi", age: 24 },
     ];
+    // ACT
     const expected: ReadonlyArray<string>= ["Aron", "Stormi"];
+    // ASSERT
     const result = justNames(PEOPLE_LIST);
     expect(result).toEqual(expected);
   })
 });
 
-
-// 1. DONE: copy people array w/ spread operator
-// 2. DONE: add age to agelessPeople w/ map()+spread operator
-// 3. DONE: remvove age from people w/ map(), return array of objects w/out age key
-// 4. DONE: remove age from people w/ map() && destructuring, return array of objects w/out age key
-// 5. DONE: remove age and return just object with name w/ destructing + map
-// 6. DONE: given people array, return an array of objects with id key w/ value of the name w/ destructuring and map()
-// 7. DONE: given people array, return array of strings of the names
