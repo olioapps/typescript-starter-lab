57bf5066735f27fe583c61b449994464fa2f54aa
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-21 14:39:17 -0700

    write initial tests and stub functions

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,26 @@
-//write tests here
+describe('getAllUsers', () => {
+  it('should return the array of users', () => {
+    const user1 = new User("Scorpo Fangoria", 16, "puce", 1);
+    const user2 = new User("Cadder Jenks", 67, "Barbie", 2);
+    const user_repository = [user1, user2];
+    const userAPI = new UserAPI(user_repository);
+    const expected = [
+      {
+        name: "Scorpo Fangoria",
+        age: 16,
+        favColor: "puce",
+        id: 1
+      },
+      {
+        name: "Cadder Jenks",
+        age: 67,
+        favColor: "Barbie",
+        id: 2
+      }
+    ]
 
-describe('Tests will go here!', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy()
+    const actual = userAPI.getAllUsers();
+
+    expect(actual).toEqual(expected);
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-//Define class here
+export class User {
+  constructor(
+    public name: string, 
+    public age: number,
+    public favColor: string,
+    private id: number
+  ){}
+}
+
+export class UserAPI {
+  public users: User[] = [];
+
+  constructor(initialUsers?: User[]) {
+    if(initialUsers) {
+      this.users = initialUsers;
+    }
+  }
