30267ce3fc55f577ac2cbf2749e0bc266f1e5191
Emma Gerigscott <elgerig@gmail.com>
2023-08-22 11:29:47 -0700

    prettier clean up

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { UserAPI, User,} from ".";
+import { UserAPI, User } from ".";
 import userRepo from "./mockdata";
 
 describe("UserAPI constructor", () => {
@@ -9,19 +9,19 @@ describe("UserAPI constructor", () => {
     expect(actual).toEqual(expected);
   });
 
-  it('should instantiate an instance of UserApi with a user repository', () => {
+  it("should instantiate an instance of UserApi with a user repository", () => {
     const newUserApi = new UserAPI(userRepo);
     const expected = Object.keys(userRepo).length;
     const actual = newUserApi.getAllUsers().length;
-    expect(actual).toEqual(expected)
+    expect(actual).toEqual(expected);
   });
 });
 
-describe('UserAPI.getAllUsers', () => {
-  it('should return an array of all users in the repository', () => {
+describe("UserAPI.getAllUsers", () => {
+  it("should return an array of all users in the repository", () => {
     const newUserApi = new UserAPI(userRepo);
     const expected = Object.values(userRepo);
     const actual = newUserApi.getAllUsers();
     expect(actual).toEqual(expected);
-  })
-})
+  });
+});
