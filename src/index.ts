054cf659821524199581269ac0e45f5415e6528a
Emma Gerigscott <elgerig@gmail.com>
2023-08-24 14:16:24 -0700

    format

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 type EventObject = {
-  timestamp: number,
-  eventType: string,
-}
+  timestamp: number;
+  eventType: string;
+};
 
 type Score = {
-  subregion: Array<EventObject>,
-  score: number,
-}
+  subregion: Array<EventObject>;
+  score: number;
+};
 
-const eventScore = (events: Array<EventObject>, regionLength: number = 5): Score => {
+const eventScore = (
+  events: Array<EventObject>,
+  regionLength: number = 5
+): Score => {
   return {
     subregion: [],
-    score: 0
-  }
-}
+    score: 0,
+  };
+};
 
 export { eventScore, EventObject };
