717fcf58877838fa9aefc9315a75e9cc7e2e1e77
Emma Gerigscott <elgerig@gmail.com>
2023-08-24 10:20:09 -0700

    add mock data

---
+++ src/mock_data.ts
@@ -0,0 +1,40 @@
+const eventData = [
+  {
+    timestamp: 123123123,
+    eventType: "newMessage",
+  },
+  {
+    timestamp: 123123124,
+    eventType: "newMessage",
+  },
+  {
+    timestamp: 123123125,
+    eventType: "newMessage",
+  },
+  {
+    timestamp: 123123125,
+    eventType: "view",
+  },
+  {
+    timestamp: 123123125,
+    eventType: "view",
+  },
+  {
+    timestamp: 123123125,
+    eventType: "screenshot",
+  },
+  {
+    timestamp: 123123125,
+    eventType: "screenshot",
+  },
+  {
+    timestamp: 123123125,
+    eventType: "newMessage",
+  },
+  {
+    timestamp: 123123125,
+    eventType: "newMessage",
+  },
+];
+
+export { eventData }
