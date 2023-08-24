47f54f5000b67f4a769c6820e9756a321e5b6132
Sarah Reimann <reimannsarah45@gmail.com>
2023-08-24 15:43:49 -0700

    write failing tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,51 @@
-//write tests here
+import { eventStream, region1, region2, region3, region4, eventStreamWithScores } from './test_data';
+import { scoreEventStream } from '.';
 
-describe('Tests will go here!', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy()
+describe('scoreEventStream', () => {
+  it('should return an object with an events property', () => {
+    const actual = scoreEventStream(eventStream);
+
+    expect(actual).toHaveProperty("events");
+  })
+
+  it('should return an object with a score property', () => {
+    const actual = scoreEventStream(eventStream);
+
+    expect(actual).toHaveProperty("score");
+  })
+  
+  it('successfully assess the length of the inputted array', () => {
+    const expected = 5;
+
+    const actual = scoreEventStream(eventStream);
+
+    expect(actual).toEqual(expected);
+  })
+
+  it('should return an object whos events property is the expected test region', () => {
+    const expected = {
+      events: region1,
+      score: 0
+    }
+
+    const actual = scoreEventStream(region1);
+
+    expect(actual).toEqual(expected);
+  })
+
+  it('should return an immutable copy of the eventStream where each event has a new score property', () => {
+    const expected = eventStreamWithScores;
+
+    const actual = scoreEventStream(eventStream);
+
+    expect(actual).toEqual(expected);
+  })
+
+  it('should return an object with the cumulative score of the region as the score property', () => {
+    const expected = 7;
+
+    const actual = scoreEventStream(region1);
+
+    expect(actual).toEqual(expected);
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-//Define class/functions here
+type event = {
+  timestamp: number,
+  eventType: string
+}
+
+type highest_score = {
+  events: ReadonlyArray<event>,
+  score: number
+}
+
+export function scoreEventStream(eventStream: ReadonlyArray<event>): highest_score {
+  return {
+    events: [],
+    score: 0
+  }
+}
