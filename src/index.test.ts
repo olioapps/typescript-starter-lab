93bb9e795df4295789dcf869d8c63dc5ad9145ab
Emma Gerigscott <elgerig@gmail.com>
2023-08-24 13:27:36 -0700

    add test 2 for default length

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,15 @@
-import { eventData } from "./mock_data"
-import eventStream from "."
+import { eventData } from "./mock_data";
+import { eventStream } from ".";
 
-describe('eventStream', () => {
-  it('should return an array of scores', () => {
-    expect().toEqual()
+describe("eventStream", () => {
+  it("should return an array of scores", () => {
+    const expected = 9;
+    const actual = eventStream(eventData, 1);
+    expect(actual).toEqual(expected);
+  });
+  it("should have a default regionLength of 5 if no region length is given", () => {
+    const expected = 5;
+    const actual = eventStream(eventData)[0].subregion.length;
+    expect(actual).toEqual(expected);
   })
-})
+});
