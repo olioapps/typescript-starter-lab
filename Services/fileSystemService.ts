2aa17bb4904e3b9fe6bc5195b19b4cf3c44ec63c
Emma Gerigscott <elgerig@gmail.com>
2023-08-30 13:53:33 -0700

    add read methods

--- Services/fileSystemService.ts
+++ Services/fileSystemService.ts
@@ -1,8 +1,37 @@
 import * as fs from "fs";
 
 export class FileSystemService {
-  readUserDataFile() {
-    return {};
+  readAllUserDataFiles() {
+    return new Promise((resolve, reject) => {
+      fs.readdir("./data", (err, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          const promises = files.map((fileName) => {
+            return this.readUserDataFile(fileName.split(".")[0]);
+          });
+          Promise.all(promises)
+            .then((data) => {
+              resolve(data);
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        }
+      });
+    });
+  }
+
+  readUserDataFile(userId: string) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(`./data/${userId}.json`, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(data.toString()));
+        }
+      });
+    });
   }
 
   writeUserDataFile() {}
