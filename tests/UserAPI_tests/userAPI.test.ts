39ea54eee868505f5e67fcf98737761e21ea1f28
Emma Gerigscott <elgerig@gmail.com>
2023-09-01 15:14:49 -0700

    add all tests for userAPI
    Co-authored-by: Sarah Reimann <reimannsarah@users.noreply.github.com>

--- tests/UserAPI_tests/userAPI.test.ts
+++ tests/UserAPI_tests/userAPI.test.ts
@@ -1,165 +1,51 @@
-import { UserAPI } from "../../Services/userAPIService";
-// import { userRepo, newUser } from "./mockdata";
+import { UserAPI, FileSystemService } from "../../Services/userAPIService";
+import { userRepo, newUser } from "./mockdata";
 
-describe("UserAPI constructor", () => {
-  it("should ", () => {
-    expect(true).toBeTruthy();
+describe("UserAPI", () => {
+  let userApi: UserAPI;
+  let fileSystemServiceMock: FileSystemService;
+
+  beforeEach(() => {
+    fileSystemServiceMock = {
+      readAllUserDataFiles: jest.fn().mockResolvedValue(userRepo),
+      readUserDataFile: jest.fn().mockResolvedValue(userRepo["342"]),
+      writeUserDataFile: jest.fn().mockResolvedValue(newUser),
+      updateUserDataFile: jest.fn().mockResolvedValue(userRepo["342"]),
+      deleteUserDataFile: jest.fn().mockResolvedValue("342"),
+    };
+    userApi = new UserAPI(fileSystemServiceMock);
+  });
+
+  it("should return all users", async () => {
+    const users = await userApi.getAllUsers();
+    expect(users).toEqual(Object.values(userRepo));
+  });
+
+  it("should return a user by id", async () => {
+    const user = await userApi.getUserById("342");
+    expect(user).toEqual(userRepo["342"]);
+  });
+
+  it("should add a user", async () => {
+    const user = await userApi.addUser(newUser);
+    let actual;
+    if (user) {
+      actual = user.name;
+    }
+    expect(actual).toEqual(newUser.name);
+  });
+
+  it("should update a user", async () => {
+    const updateUserData = { name: "Pancake", age: 14, favColor: "blue" };
+
+    const user = await userApi.updateUserById("342", updateUserData);
+    expect(user).toEqual({ ...updateUserData, id: "342" });
+  });
+
+  it("should delete a user", async () => {
+    const userId = "342";
+    const expected = await userApi.getUserById(userId);
+    const user = await userApi.deleteUserById(userId);
+    expect(user).toEqual(expected);
   });
 });
-// describe("UserAPI constructor", () => {
-//   it("should instantiate an instance of UserAPI without user repository", () => {
-//     const newUserApi = new UserAPI();
-//     const expected = 0;
-//     const actual = newUserApi.getAllUsers().length;
-//     expect(actual).toEqual(expected);
-//   });
-
-//   it("should instantiate an instance of UserApi with a user repository", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const expected = Object.keys(userRepo).length;
-//     const actual = newUserApi.getAllUsers().length;
-//     expect(actual).toEqual(expected);
-//   });
-// });
-
-// describe("UserAPI.getAllUsers", () => {
-//   it("should return an array of all users in the repository", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const expected = Object.values(userRepo);
-//     const actual = newUserApi.getAllUsers();
-//     expect(actual).toEqual(expected);
-//   });
-// });
-
-// describe("UserAPI.addUser", () => {
-//   it("should add uninitialized user to the UserAPI repo", () => {
-//     const newUserApi = new UserAPI();
-//     newUserApi.addUser(newUser);
-//     const expected = 1;
-//     const actual = newUserApi.getAllUsers().length;
-//     expect(actual).toEqual(expected);
-//   });
-//   it("should generate an id for a new user when added to the UserAPI repo", () => {
-//     const newUserApi = new UserAPI();
-//     newUserApi.addUser(newUser);
-//     expect(newUserApi.getAllUsers()[0].id).toBeTruthy();
-//   });
-//   it("should append new user to existing repo", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     newUserApi.addUser(newUser);
-//     const expected = 4;
-//     const actual = newUserApi.getAllUsers().length;
-//     expect(actual).toEqual(expected);
-//   });
-//   it("should return the new user", () => {
-//     const newUserApi = new UserAPI();
-//     newUserApi.addUser(newUser);
-//     const expected = newUser.name;
-//     const actual = newUserApi.addUser(newUser).name;
-//     expect(actual).toEqual(expected);
-//   });
-// });
-
-// describe("UserAPI.getUserById", () => {
-//   it("should return a single user object when matching id string is passed as an argument", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const expected = newUserApi.getAllUsers()[0];
-//     const actual = newUserApi.getUserById("342");
-//     expect(actual).toEqual(expected);
-//   });
-//   it("should throw an error if no id matches a user", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const error = () => newUserApi.getUserById("");
-//     expect(error).toThrow("User not found");
-//   });
-// });
-
-// describe("UserAPI.deleteUserById", () => {
-//   it("should remove a user from a larger repo when given an id string", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const expected = "721";
-//     const actual = newUserApi.deleteUserById("721").id;
-//     expect(actual).toEqual(expected);
-//   });
-//   it("should return the deleted user", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const expected = newUserApi.getUserById("342");
-//     const actual = newUserApi.deleteUserById("342");
-//     expect(actual).toEqual(expected);
-//   });
-//   it("should throw an error if invalid user id was passed", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const error = () => newUserApi.deleteUserById("");
-//     expect(error).toThrow("User not found");
-//   });
-// });
-
-// describe("UserAPI.updateUser", () => {
-//   it("should update a user at a given id with the user object passed", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const updateUserInfo = {
-//       name: "Sandle",
-//       age: 15,
-//       favColor: "grey",
-//     };
-//     const expected = "Sandle";
-
-//     const updatedUser = newUserApi.updateUserById("342", updateUserInfo);
-//     const actual = updatedUser.name;
-
-//     expect(actual).toEqual(expected);
-//   });
-//   it("should throw an error if invalid user id was passed", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const error = () => newUserApi.updateUserById("", newUser);
-
-//     expect(error).toThrow("User not found");
-//   });
-// });
-
-// describe("UserAPI.searchUsersByName", () => {
-//   it("should return an array of users with matching names", () => {
-//     const newUserApi = new UserAPI(userRepo);
-
-//     const actual = newUserApi.searchUsersByName("Pi");
-//     const expected = [
-//       {
-//         name: "Pi",
-//         age: 15,
-//         favColor: "grey",
-//         id: "342",
-//       },
-//     ];
-
-//     expect(actual).toEqual(expected);
-//   });
-
-//   it("should return an array of all users with matching letters in the name", () => {
-//     const newUserApi = new UserAPI(userRepo);
-
-//     const actual = newUserApi.searchUsersByName("p");
-//     const expected = [{ ...userRepo[342] }, { ...userRepo[721] }];
-
-//     expect(actual).toEqual(expected);
-//   });
-// });
-
-// describe("UserAPI.searchUsersByFavColor", () => {
-//   it("should return an array of users with matching favorite colors", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const expected = [{ ...userRepo[465] }];
-
-//     const actual = newUserApi.searchUsersByFavColor("pink");
-
-//     expect(actual).toEqual(expected);
-//   });
-
-//   it("should return an array of all users with matching letters in favColor", () => {
-//     const newUserApi = new UserAPI(userRepo);
-//     const expected = [{ ...userRepo[342] }, { ...userRepo[721] }];
-
-//     const actual = newUserApi.searchUsersByFavColor("e");
-
-//     expect(actual).toEqual(expected);
-//   })
-// });
