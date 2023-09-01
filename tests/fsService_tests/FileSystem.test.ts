e7bee7db6c3b783874c88869ef4df586a71cf0dc
Sarah Reimann <reimannsarah45@gmail.com>
2023-09-01 13:19:35 -0700

    add tests for readUserDateFile

--- tests/fsService_tests/FileSystem.test.ts
+++ tests/fsService_tests/FileSystem.test.ts
@@ -1,4 +1,3 @@
-
 import { fileSystemService } from "../../Services/fileSystemService";
 import * as fs from "fs";
 const path = require("path");
@@ -7,22 +6,94 @@ describe("fileSystemService.readAllUserDataFiles", () => {
   const dataDir = path.join(__dirname, "../../data");
 
   beforeEach(async () => {
-    await fs.writeFile(path.join(dataDir, "test1.json"), JSON.stringify({ name: "test1" , age: 34, favColor: "vole", id: "snale"}), () => { });
-    await fs.writeFile(path.join(dataDir, "test2.json"), JSON.stringify({ name: "test2" , age: 34, favColor: "clear puce", id: "snake"}), () => { });
+    await fs.writeFile(
+      path.join(dataDir, "123.json"),
+      JSON.stringify({ name: "test1", age: 34, favColor: "vole", id: "123" }),
+      () => {}
+    );
+    await fs.writeFile(
+      path.join(dataDir, "456.json"),
+      JSON.stringify({
+        name: "test2",
+        age: 34,
+        favColor: "clear puce",
+        id: "456",
+      }),
+      () => {}
+    );
   });
 
   afterEach(async () => {
-    await fs.unlink(path.join(dataDir, "test1.json"), () => { });
-    await fs.unlink(path.join(dataDir, "test2.json"), () => { });
+    await fs.unlink(path.join(dataDir, "123.json"), () => {});
+    await fs.unlink(path.join(dataDir, "456.json"), () => {});
   });
 
   it("should return all users", async () => {
     const users = await fileSystemService.readAllUserDataFiles();
     expect(users).toEqual([
-      { name: "test1" , age: 34, favColor: "vole", id: "snale"},
-      { name: "test2" , age: 34, favColor: "clear puce", id: "snake"},
+      { name: "test1", age: 34, favColor: "vole", id: "123" },
+      { name: "test2", age: 34, favColor: "clear puce", id: "456" },
     ]);
   });
 });
 
+describe("fileSystemService.readUserDataFile", () => {
+  const dataDir = path.join(__dirname, "../../data");
 
+  beforeEach(async () => {
+    await fs.writeFile(
+      path.join(dataDir, "123.json"),
+      JSON.stringify({ name: "test1", age: 34, favColor: "vole", id: "123" }),
+      () => {}
+    );
+    await fs.writeFile(
+      path.join(dataDir, "456.json"),
+      JSON.stringify({
+        name: "test2",
+        age: 34,
+        favColor: "clear puce",
+        id: "456",
+      }),
+      () => {}
+    );
+  });
+
+  afterEach(async () => {
+    await fs.unlink(path.join(dataDir, "123.json"), () => {});
+    await fs.unlink(path.join(dataDir, "456.json"), () => {});
+    await fs.unlink(path.join(dataDir, "789.json"), () => {});
+  });
+
+  it("should return a user", async () => {
+    const user = await fileSystemService.readUserDataFile("123");
+    expect(user).toEqual({
+      name: "test1",
+      age: 34,
+      favColor: "vole",
+      id: "123",
+    });
+  });
+
+  it("should throw an error if the file doesn't exist", async () => {
+    try {
+      await fileSystemService.readUserDataFile("789");
+    } catch (err) {
+      const error = err as NodeJS.ErrnoException;
+      expect(error.code).toEqual("ENOENT");
+    }
+  });
+  
+  it("should throw an error if the file is not valid JSON", async () => {
+    try {
+      await fs.writeFile(
+        path.join(dataDir, "789.json"),
+        "invalid json",
+        () => {}
+      );
+      await fileSystemService.readUserDataFile("789");
+    } catch (err) {
+      const error = err as NodeJS.ErrnoException;
+      expect(error.code).toEqual("ENOENT");
+    }
+  });
+});
